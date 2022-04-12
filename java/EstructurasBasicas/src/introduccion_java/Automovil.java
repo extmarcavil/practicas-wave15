f8ed9e83f1b3c9f057038d74eb89ea9c9bbbef44
= <=>
2022-04-11 21:48:53 -0500

    Ejercicios de introduccion al lenguaje java, poo

---
+++ java/EstructurasBasicas/src/introduccion_java/Automovil.java
@@ -0,0 +1,23 @@
+package introduccion_java;
+
+public class Automovil {
+
+    String marca;
+    String color;
+    double kilometros;
+
+    public Automovil() {
+
+    }
+
+    public Automovil(String marca, String color, double kilometros) {
+        this.marca = marca;
+        this.color = color;
+        this.kilometros = kilometros;
+    }
+
+    public String mostrarMarcaYColor() {
+        String marcaYColor = "La marca del auto es: " + this.marca + ". El color del auto es: " + this.color;
+        return mostrarMarcaYColor();
+    }
+}
