a81b868e0e3c601659e97145946aea6f55ee1573
IFerrante-meli <ignacio.ferrante@mercadolibre.com>
2022-04-12 12:03:47 -0300

    ejercicio lambdas

---
+++ java/genericsWrapperLambdaStrem/src/Garage.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garage {
+    private int id;
+    private List<Vehiculo> vehiculos;
+
+    public Garage(int id, List<Vehiculo> vehiculos) {
+        this.id = id;
+        this.vehiculos = vehiculos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+}
