69cc6f0168e158c9a6286b04ced19e1fa110347e
Lucas Jimenez <lucas.jimenez@mercadolibre.com>
2022-04-13 21:03:18 -0300

    Completo SaveTheRopa

--- java/integradorP2Vivo/src/saveTheRopa/modelo/GuardaRopa.java
+++ java/integradorP2Vivo/src/saveTheRopa/modelo/GuardaRopa.java
@@ -1,5 +1,6 @@
 package saveTheRopa.modelo;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,12 +11,27 @@ public class GuardaRopa {
 
     public GuardaRopa() {
         contador=0;
+        map=new HashMap<>();
     }
 
     public Integer guardarPrendas(List<Prenda> listDePrendas){
         contador++;
         map.put(contador,listDePrendas);
         return contador;
+    }
+
+    public void mostrarPrendas(){
+        System.out.println("=======================================");
+        System.out.println("Prendas almacenadas en el GuardaRopas: ");
+        System.out.println(map);
+        System.out.println("=======================================");
+    }
+
+    public List<Prenda> devolverPrendas(Integer numero){
+        List<Prenda> list= map.remove(numero);
+        if (list == null) System.out.println("No pudimos localizar sus prendas :(");
+        else System.out.println("Prendas devueltas con exito.");
+        return list;
     }
 
 }
--- java/integradorP2Vivo/src/saveTheRopa/modelo/Main.java
+++ java/integradorP2Vivo/src/saveTheRopa/modelo/Main.java
@@ -1,5 +1,26 @@
 package saveTheRopa.modelo;
 
-public class Main {
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+
+        GuardaRopa guardaRopa=new GuardaRopa();
+        List<Prenda> prendaList=List.of(
+                new Prenda("Kike","Lordan"),
+                new Prenda("Adila","Oligen")
+        );
+        guardaRopa.guardarPrendas(prendaList);
+
+        List<Prenda> list=List.of(
+                new Prenda("Kike","Lordan")
+        );
+        guardaRopa.guardarPrendas(list);
+        guardaRopa.mostrarPrendas();
+
+        System.out.println(guardaRopa.devolverPrendas(1));
+        guardaRopa.devolverPrendas(3);
+
+
+    }
 }
