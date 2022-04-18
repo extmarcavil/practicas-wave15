b436b36bf335e42b14e2c777c8df3e319889e6a9
Facundo Cardenas <facundo.cardenas@mercadolibre.com>
2022-04-18 16:39:33 -0300

    first project on spring boot

---
+++ spring/firstproject/src/test/java/com/firstproject/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
