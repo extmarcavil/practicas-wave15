04dc8b44183cd790d03390fcef6e47da7dfb671f
mfranciscog <marcos.giaccaglia@mercadolibre.com>
2022-04-18 15:19:51 -0300

    adicion proyectos

---
+++ java/AgenciaDeTurismo/src/Tickets.java
@@ -0,0 +1,11 @@
+public class Tickets extends Producto implements Reserva{
+
+    public Tickets(int precio, int cantidad){
+        super(precio, cantidad);
+    }
+
+    public void reservar(){
+
+    }
+
+}
--- java/Dakar/src/Carrera.java
+++ java/Dakar/src/Carrera.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-
+               
 public class Carrera {
     private double distancia;
     private int cantidadDeVehiculosPermitidos;
