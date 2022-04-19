ab9a1f2b80fc8d30edd3ee88c1db942496444475
Thiago Ramirez <thiago.ramirez@mercadolibre.com>
2022-04-19 17:14:39 -0300

    Ejercicio numeros romanos

---
+++ spring/EjercicioNumerosRomanos/src/main/java/com/example/EjercicioNumerosRomanos/EjercicioNumerosRomanosApplication.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioNumerosRomanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioNumerosRomanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioNumerosRomanosApplication.class, args);
+	}
+
+}
---
+++ spring/EjercicioNumerosRomanos/src/test/java/com/example/EjercicioNumerosRomanos/EjercicioNumerosRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioNumerosRomanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioNumerosRomanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
