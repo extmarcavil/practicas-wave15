5b75aa61d660bc925be31de3676363e7b360edd2
nicookazan <kazandjiannicolas@gmail.com>
2022-04-19 15:54:49 -0300

    Convertidos de números romanos
    Convertidor de números romanos

---
+++ spring/romanconverter/src/main/java/com/romanconverter/controller/RomanConverterController.java
@@ -0,0 +1,35 @@
+package com.romanconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.TreeMap;
+
+@RestController
+public class RomanConverterController {
+    @GetMapping("/convert/{number}")
+    public String convertToRomanNumber(@PathVariable int number){
+        TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+        int l =  map.floorKey(number);
+        if ( number == l ) {
+            return map.get(number);
+        }
+        return map.get(l) + convertToRomanNumber(number-l);
+    }
+}
---
+++ spring/romanconverter/src/test/java/com/romanconverter/RomanconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
