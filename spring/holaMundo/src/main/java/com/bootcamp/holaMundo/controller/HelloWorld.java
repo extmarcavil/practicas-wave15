b846da72490b7b71380c1d0a8a34f6e0f613b13d
mfranciscog <marcos.giaccaglia@mercadolibre.com>
2022-04-19 17:04:45 -0300

    adicion proyectos

--- spring/holaMundo/src/main/java/com/bootcamp/holaMundo/controller/HelloWorld.java
+++ spring/holaMundo/src/main/java/com/bootcamp/holaMundo/controller/HelloWorld.java
@@ -4,12 +4,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/hello/{name}")
-    public String hello(@PathVariable String name){
+    @GetMapping("/{number}")
+    public String convertToRoman(@PathVariable Integer number){
 
-        return "Hola " + name + "!";
+        Map<Integer,String> listaNumeros = new HashMap<>();
+        listaNumeros.put(1,"I");
+        listaNumeros.put(2,"II");
+        listaNumeros.put(3,"III");
+        listaNumeros.put(4,"IV");
+        listaNumeros.put(5,"V");
+        listaNumeros.put(7,"VII");
+        listaNumeros.put(10,"X");
+        listaNumeros.put(13,"XIII");
+        listaNumeros.put(50,"L");
+        listaNumeros.put(100,"C");
+        listaNumeros.put(500,"D");
+        listaNumeros.put(1000,"M");
+
+        if(listaNumeros.containsKey(number))
+            return listaNumeros.get(number);
+
+        else{return "No se puede transformar tal número";}
     }
 }
