e0297d9ad6c4f0acc9516bdebc20288860aa8e81
Julieta Leonetti <julieta.leonetti@mercadolibre.com>
2022-04-19 16:08:57 -0300

    ADD NUM ROMANOS

---
+++ java/numerosRomanos/src/main/java/com/example/numerosRomanos/NumerosRomanosApplication.java
@@ -0,0 +1,14 @@
+package com.example.numerosRomanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerosRomanosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NumerosRomanosApplication.class, args);
+	}
+
+}
