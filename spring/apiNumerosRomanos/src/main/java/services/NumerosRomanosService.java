c80dcfdfa2a3417a6903f1ceb7480acc4bdda7ff
Gerson Jair Sanchez Arroyo <gesanchez@mx0c02fr2dtq05p.ml.com>
2022-04-19 12:43:37 -0500

    ejercicio de api de convertir un numero decimal a romano ,PG  Spring-P2

---
+++ spring/apiNumerosRomanos/src/main/java/services/NumerosRomanosService.java
@@ -0,0 +1,71 @@
+package services;
+
+public class NumerosRomanosService {
+
+    //region Metodos
+    public String convertDecimalRomano(int numeroDecimal){
+        int i, miles, centenas, decenas, unidades;
+        String romano = "";
+
+        //obtenemos cada cifra del número
+        miles = numeroDecimal / 1000;
+        centenas = numeroDecimal / 100 % 10;
+        decenas = numeroDecimal / 10 % 10;
+        unidades = numeroDecimal % 10;
+
+        //millar
+        for (i = 1; i <= miles; i++) {
+            romano = romano + "M";
+        }
+
+        //centenas
+        if (centenas == 9) {
+            romano = romano + "CM";
+        } else if (centenas >= 5) {
+            romano = romano + "D";
+            for (i = 6; i <= centenas; i++) {
+                romano = romano + "C";
+            }
+        } else if (centenas == 4) {
+            romano = romano + "CD";
+        } else {
+            for (i = 1; i <= centenas; i++) {
+                romano = romano + "C";
+            }
+        }
+
+        //decenas
+        if (decenas == 9) {
+            romano = romano + "XC";
+        } else if (decenas >= 5) {
+            romano = romano + "L";
+            for (i = 6; i <= decenas; i++) {
+                romano = romano + "X";
+            }
+        } else if (decenas == 4) {
+            romano = romano + "XL";
+        } else {
+            for (i = 1; i <= decenas; i++) {
+                romano = romano + "X";
+            }
+        }
+
+        //unidades
+        if (unidades == 9) {
+            romano = romano + "IX";
+        } else if (unidades >= 5) {
+            romano = romano + "V";
+            for (i = 6; i <= unidades; i++) {
+                romano = romano + "I";
+            }
+        } else if (unidades == 4) {
+            romano = romano + "IV";
+        } else {
+            for (i = 1; i <= unidades; i++) {
+                romano = romano + "I";
+            }
+        }
+        return romano;
+    }
+    //endregion
+}
---
+++ spring/apiNumerosRomanos/src/test/java/com/apinumerosromanos/ApiNumerosRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apinumerosromanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNumerosRomanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
