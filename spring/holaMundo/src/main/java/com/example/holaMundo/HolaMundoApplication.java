5a8d136a8ff6db46d5eaee9abe124028d6be0888
Sebastian Elias Muchut <sebastian.muchut@mercadolibre.com>
2022-04-19 15:54:18 -0300

    holamundo

---
+++ spring/holaMundo/src/main/java/com/example/holaMundo/HolaMundoApplication.java
@@ -0,0 +1,14 @@
+package com.example.holaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
