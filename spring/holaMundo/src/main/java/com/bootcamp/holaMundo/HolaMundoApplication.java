9e4c1ede66ef61b9ba1682d0645f787f3a8cba89
mfranciscog <marcos.giaccaglia@mercadolibre.com>
2022-04-19 12:17:17 -0300

    adicion proyectos

---
+++ spring/holaMundo/src/main/java/com/bootcamp/holaMundo/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.holaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
---
+++ spring/holaMundo/src/main/java/com/bootcamp/holaMundo/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.bootcamp.holaMundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name){
+
+        return "Hola " + name + "!";
+    }
+}
