e0d9fb45f04e04e4dfadd69e3d5c10a79a0607ac
malvarez22 <matiassebastian.alvarez@mercadolibre.com>
2022-04-19 09:19:16 -0300

    Clases Abstractas e Interfaces - VIVO - Ejercicio del Banco

---
+++ java/Banco/src/Deposito.java
@@ -0,0 +1,11 @@
+public class Deposito implements Transaccion{
+    @Override
+    public void transaccionOk() {
+
+    }
+
+    @Override
+    public void transaccionNoOk() {
+
+    }
+}
---
+++ java/Clases Abstractas e Interfaces - VIVO/Banco/src/Transaccion.java
@@ -0,0 +1,4 @@
+public interface Transaccion {
+    public void transaccionOk();
+    public void transaccionNoOk();
+}
