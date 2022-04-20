8aa127657e3e5767c44ded938843c0af8559689b
Nicolás <nicolas.santos@mercadolibre.com>
2022-04-20 09:39:51 -0300

    Decimal number to Roman with Spring

---
+++ spring/DecimalToRoman/src/main/java/com/example/roman/RomanApplication.java
@@ -0,0 +1,13 @@
+package com.example.roman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanApplication.class, args);
+    }
+
+}
---
+++ spring/DecimalToRoman/src/test/java/com/example/roman/RomanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
