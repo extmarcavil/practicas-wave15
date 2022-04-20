d27c2be299b1bf88e0d07468034599694c6b92c0
Luis David Rodriguez Benitez <luisrodrig@mx0c02fr2cdq05p.ml.com>
2022-04-20 07:59:02 -0500

    agregar práctica de Código Morse

---
+++ spring/morse/src/main/java/com/practicas/morse/MorseApplication.java
@@ -0,0 +1,13 @@
+package com.practicas.morse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorseApplication.class, args);
+    }
+
+}
---
+++ spring/morse/src/main/java/com/practicas/morse/MorseController.java
@@ -0,0 +1,53 @@
+package com.practicas.morse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MorseController {
+    public static Map<String, String> caracteres;
+    static {
+        caracteres = new HashMap<>();
+        caracteres.put(".-", "A");caracteres.put("-...", "B");
+        caracteres.put("-.-.", "C");caracteres.put("-..", "D");
+        caracteres.put(".", "E");caracteres.put("..-.", "F");
+        caracteres.put("--.", "G");caracteres.put("....", "H");
+        caracteres.put("..", "I");caracteres.put(".---", "J");
+        caracteres.put("-.-", "K");caracteres.put(".-..", "L");
+        caracteres.put("--", "M");caracteres.put("-.", "N");
+        caracteres.put("---", "O");caracteres.put(".--.", "P");
+        caracteres.put("--.-", "Q");caracteres.put(".-.", "R");
+        caracteres.put("...", "S");caracteres.put("-", "T");
+        caracteres.put("..-", "U");caracteres.put("...-", "V");
+        caracteres.put(".--", "W");caracteres.put("-..-", "X");
+        caracteres.put("-.--", "Y");caracteres.put("--..", "Z");
+        caracteres.put(".----", "1");caracteres.put("..---", "2");
+        caracteres.put("...--", "3");caracteres.put("....-", "4");
+        caracteres.put(".....", "5");caracteres.put("-....", "6");
+        caracteres.put("--...", "7");caracteres.put("---..", "8");
+        caracteres.put("----.", "9");caracteres.put("-----", "0");
+        caracteres.put("..--..", "?");caracteres.put("-.-.--", "!");
+        caracteres.put(".-.-.-", ".");caracteres.put("--..--", ",");
+
+    }
+
+    @GetMapping("morse/{cadena}")
+    public String getString(@PathVariable String cadena){
+        String resultado = "";
+        for (String parte: cadena.split("   ")) {
+            for (String caracter: parte.split(" ")) {
+                String temp = caracteres.get(caracter);
+                if(temp == null){
+                    temp = "";
+                }
+                resultado = resultado + temp;
+            }
+            resultado = resultado + " ";
+        }
+        return resultado;
+    }
+}
