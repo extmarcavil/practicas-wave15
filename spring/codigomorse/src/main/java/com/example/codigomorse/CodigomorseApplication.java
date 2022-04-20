ce2dae7538dc580189e0e9abfe3d0cf32739cde6
Gomez Rodrigo <rodrigogabriel.gomez@mercadolibre.com>
2022-04-20 15:08:17 -0300

    morse

---
+++ spring/codigomorse/src/main/java/com/example/codigomorse/CodigomorseApplication.java
@@ -0,0 +1,13 @@
+package com.example.codigomorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigomorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigomorseApplication.class, args);
+	}
+
+}
