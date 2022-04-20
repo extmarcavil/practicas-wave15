82aa5eb0606cb75f7e97bc69184a76a867bf02cb
Nicolás <nicolas.santos@mercadolibre.com>
2022-04-20 11:16:49 -0300

    Spring Practice

---
+++ spring/Spring/src/main/java/com/example/spring/controller/MorseController.java
@@ -0,0 +1,87 @@
+package com.example.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/morse")
+public class MorseController {
+
+    public static Map<String, String> obtenerEquivalencias() {
+        Map<String, String> equivalencias = new Hashtable<>();
+        equivalencias.put("A", ".-");
+        equivalencias.put("B", "-...");
+        equivalencias.put("C", "-.-.");
+        equivalencias.put("CH", "----");
+        equivalencias.put("D", "-..");
+        equivalencias.put("E", ".");
+        equivalencias.put("F", "..-.");
+        equivalencias.put("G", "--.");
+        equivalencias.put("H", "....");
+        equivalencias.put("I", "..");
+        equivalencias.put("J", ".---");
+        equivalencias.put("K", "-.-");
+        equivalencias.put("L", ".-..");
+        equivalencias.put("M", "--");
+        equivalencias.put("N", "-.");
+        equivalencias.put("Ñ", "--.--");
+        equivalencias.put("O", "---");
+        equivalencias.put("P", ".--.");
+        equivalencias.put("Q", "--.-");
+        equivalencias.put("R", ".-.");
+        equivalencias.put("S", "...");
+        equivalencias.put("T", "-");
+        equivalencias.put("U", "..-");
+        equivalencias.put("V", "...-");
+        equivalencias.put("W", ".--");
+        equivalencias.put("X", "-..-");
+        equivalencias.put("Y", "-.--");
+        equivalencias.put("Z", "--..");
+        equivalencias.put("0", "-----");
+        equivalencias.put("1", ".----");
+        equivalencias.put("2", "..---");
+        equivalencias.put("3", "...--");
+        equivalencias.put("4", "....-");
+        equivalencias.put("5", ".....");
+        equivalencias.put("6", "-....");
+        equivalencias.put("7", "--...");
+        equivalencias.put("8", "---..");
+        equivalencias.put("9", "----.");
+        equivalencias.put(".", ".-.-.-");
+        equivalencias.put(",", "--..--");
+        equivalencias.put(":", "---...");
+        equivalencias.put("?", "..--..");
+        equivalencias.put("'", ".----.");
+        equivalencias.put("-", "-....-");
+        equivalencias.put("/", "-..-.");
+        equivalencias.put("\"", ".-..-.");
+        equivalencias.put("@", ".--.-.");
+        equivalencias.put("=", "-...-");
+        equivalencias.put("!", "−.−.−−");
+        return equivalencias;
+    }
+
+    @GetMapping("/{text}")
+    public String morseToSpanish(@PathVariable String text) {
+
+        Map<String, String> diccionario = obtenerEquivalencias();
+        StringBuilder morse = new StringBuilder();
+
+        for(String word : text.split("   ")) {
+            for(String letter : word.split(" ")) {
+                for(Map.Entry<String, String> entry : diccionario.entrySet()) {
+                    if (entry.getValue().equals(letter))
+                        morse.append(entry.getKey());
+                }
+            }
+            morse.append(" ");
+        }
+
+        return morse.toString();
+    }
+}
---
+++ spring/Spring/src/test/java/com/example/spring/RunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
