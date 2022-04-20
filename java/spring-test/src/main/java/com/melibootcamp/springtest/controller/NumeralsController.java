61bea15b3a0cf78e85afcdbad002a51906fd3c2e
leon-meli <jesusgabriel.leon@mercadolibre.com>
2022-04-20 11:01:30 -0300

    Añadido ejercicio traducción Morse -> Español

--- java/spring-test/src/main/java/com/melibootcamp/springtest/controller/NumeralsController.java
+++ java/spring-test/src/main/java/com/melibootcamp/springtest/controller/NumeralsController.java
@@ -103,4 +103,61 @@ public class NumeralsController {
     }
     */
     }
+
+    @GetMapping("/language/spanish/from_morse/{text}")
+    public String morseToText(@PathVariable String text) {
+        HashMap<String, String> caracteres = new HashMap<>();
+        caracteres.put(".-", "A");
+        caracteres.put("-...", "B");
+        caracteres.put("-.-.", "C");
+        caracteres.put("-..", "D");
+        caracteres.put(".", "E");
+        caracteres.put("..-.", "F");
+        caracteres.put("--.", "G");
+        caracteres.put("....", "H");
+        caracteres.put("..", "I");
+        caracteres.put(".---", "J");
+        caracteres.put("-.-", "K");
+        caracteres.put(".-..", "L");
+        caracteres.put("--", "M");
+        caracteres.put("-.", "N");
+        caracteres.put("---", "O");
+        caracteres.put(".--.", "P");
+        caracteres.put("--.-", "Q");
+        caracteres.put(".-.", "R");
+        caracteres.put("...", "S");
+        caracteres.put("-", "T");
+        caracteres.put("..-", "U");
+        caracteres.put("...-", "V");
+        caracteres.put(".--", "W");
+        caracteres.put("-..-", "X");
+        caracteres.put("-.--", "Y");
+        caracteres.put("--..", "Z");
+        caracteres.put(".----", "1");
+        caracteres.put("..---", "2");
+        caracteres.put("...--", "3");
+        caracteres.put("....-", "4");
+        caracteres.put(".....", "5");
+        caracteres.put("-....", "6");
+        caracteres.put("--...", "7");
+        caracteres.put("---..", "8");
+        caracteres.put("----.", "9");
+        caracteres.put("-----", "0");
+        caracteres.put("..--..", "?");
+        caracteres.put("-.-.--", "!");
+        caracteres.put(".-.-.-", ".");
+        caracteres.put("--..--", ",");
+        caracteres.put("", " ");
+
+        StringBuilder result = new StringBuilder();
+
+        String[] morseWords = text.split(" ");
+
+        for(String w : morseWords) {
+            result.append(caracteres.get(w));
+        }
+
+        return result.toString();
+
+    }
 }
