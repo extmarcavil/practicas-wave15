55e0404d8198e45ed781ca388933135eca4f301b
santiagopinzondev <santiago.pinzon@mercadolibre.com.co>
2022-04-21 11:50:22 -0500

    change sintoma

--- spring/Covid19/src/main/java/com/santiagopinzondev/Covid19/Covid19Application.java
+++ spring/Covid19/src/main/java/com/santiagopinzondev/Covid19/Covid19Application.java
@@ -7,11 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Covid19Application {
 
 	public static void main(String[] args) {
-		Sintoma gripe = new Sintoma("gripe", "gripe", 4);
-		Sintoma covid = new Sintoma("COVID19", "Covid", 10);
+		Sintoma fiebre = new Sintoma("fiebre", "fiebre", 4);
+		Sintoma cansancio = new Sintoma("cansancio", "Cansancio", 10);
 		Sintomas sintomas = new Sintomas();
-		sintomas.addSintoma(gripe);
-		sintomas.addSintoma(covid);
+		sintomas.addSintoma(fiebre);
+		sintomas.addSintoma(cansancio);
 
 		Persona santiago = new Persona(0, "santiago", "pinzon", 19);
 		Persona maria = new Persona(1, "Maria", "Perez", 62);
@@ -20,13 +20,13 @@ public class Covid19Application {
 		santiagoDTO.setNombre(santiago.getNombre());
 		santiagoDTO.setApellido(santiago.getApellido());
 		santiagoDTO.setEdad(santiago.getEdad());
-		santiagoDTO.setSintoma(gripe);
+		santiagoDTO.setSintoma(fiebre);
 
 		PersonaSintomaDTO mariaDTO = new PersonaSintomaDTO();
 		mariaDTO.setNombre(maria.getNombre());
 		mariaDTO.setApellido(maria.getApellido());
 		mariaDTO.setEdad(maria.getEdad());
-		mariaDTO.setSintoma(covid);
+		mariaDTO.setSintoma(cansancio);
 
 		PersonasDTO.addPersona(santiagoDTO);
 		PersonasDTO.addPersona(mariaDTO);
