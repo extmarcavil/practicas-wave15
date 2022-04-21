7c988a9e9bd55ceb34195dd06bf1f2bcaa8cb425
Tomas Ravelli <tomas.ravelli@mercadolibre.com>
2022-04-21 18:42:03 -0300

    starwars

---
+++ spring/starwars/src/main/java/com/example/starwars/model/Character.java
@@ -0,0 +1,23 @@
+package com.example.starwars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Character {
+    private String name;
+    private String hair_color;
+    private String skin_color;
+    private String eye_color;
+    private String birth_year;
+    private String gender;
+    private String homeworld;
+    private String species;
+    private Integer height;
+    private Integer mass;
+}
