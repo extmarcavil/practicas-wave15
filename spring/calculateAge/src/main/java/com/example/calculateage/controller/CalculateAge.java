2db5dd7be8e6142de95712c27b8cb260d619fe68
Wendy Sclerandi <wendy.sclerandi@mercadolibre.com>
2022-04-21 14:18:13 -0300

    Fix en el ejercicio de calcular edad con lo visto en clase.

--- spring/calculateAge/src/main/java/com/example/calculateage/controller/CalculateAge.java
+++ spring/calculateAge/src/main/java/com/example/calculateage/controller/CalculateAge.java
@@ -1,19 +1,48 @@
 package com.example.calculateage.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.calculateage.dto.AgeDTO;
+import com.example.calculateage.dto.bornDataDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 @RestController
+@RequestMapping("/age")
 public class CalculateAge {
 
-    @GetMapping("/{day}/{month}/{year}")
-    public int calculateAge(@PathVariable int day, @PathVariable int month, @PathVariable int year){
-        LocalDate birthDate = LocalDate.of(year,month, day);
-        LocalDate today = LocalDate.now();
-        return Period.between(birthDate, today).getYears();
+    // Ejemplo usando DTO, ResponseBody y ResponseEntity
+    @GetMapping("/calculate")
+    public ResponseEntity<AgeDTO> calculateAgePath(@RequestBody bornDataDTO bornData){
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(bornData.getBirthDate(), now);
+        return new ResponseEntity<>(new AgeDTO(period.getYears()), HttpStatus.OK);
     }
+
+    // Ejemplo base.
+
+    // @GetMapping("/calculate/{day}/{month}/{year}")
+    // public int calculateAgePath(@PathVariable Integer day,
+    //                             @PathVariable Integer month,
+    //                             @PathVariable Integer year){
+    //     return calculateAge(day, month, year);
+    // }
+    //
+    // @GetMapping("/calculate")
+    // public int calculateAgeRequest(@RequestParam Integer day,
+    //                                @RequestParam Integer month,
+    //                                @RequestParam Integer year){
+    //     return calculateAge(day, month, year);
+    // }
+    //
+    // private int calculateAge(Integer day, Integer month, Integer year){
+    //     LocalDate birthDate = LocalDate.of(year, month, day);
+    //     LocalDate today = LocalDate.now();
+    //     if (birthDate.isAfter(today)){
+    //         return 0;
+    //     }
+    //     return Period.between(birthDate, today).getYears();
+    // }
 }
