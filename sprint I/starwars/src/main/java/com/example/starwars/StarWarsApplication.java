59a4770ec063685b8cb8b26a18cfd7835f471209
gualdronsito <andres.gualdron@mercadolibre.com.co>
2022-04-21 17:28:41 -0500

    starwars exercise finished

---
+++ sprint I/starwars/src/main/java/com/example/starwars/StarWarsApplication.java
@@ -0,0 +1,11 @@
+package com.example.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(StarWarsApplication.class, args);
+  }
+}
