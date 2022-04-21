4535b5cb67a84345c07487a3db0087c8fda70126
Facundo Cardenas <facundo.cardenas@mercadolibre.com>
2022-04-20 21:01:07 -0300

    morse project

---
+++ spring/codemorse/src/main/java/com/morse/codemorse/CodemorseApplication.java
@@ -0,0 +1,13 @@
+package com.morse.codemorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemorseApplication.class, args);
+	}
+
+}
---
+++ spring/codemorse/src/test/java/com/morse/codemorse/CodemorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morse.codemorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
