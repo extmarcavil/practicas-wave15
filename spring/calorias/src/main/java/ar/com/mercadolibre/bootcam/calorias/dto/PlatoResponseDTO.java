f5027109d47ce2e037ed0d4c6350814d7e047075
gesanchez92 <gerson.sanchez@mercadolibre.com.mx>
2022-04-22 09:37:52 -0500

    api calorias ejercicios practicos P2 VIVO

---
+++ spring/calorias/src/main/java/ar/com/mercadolibre/bootcam/calorias/dto/PlatoResponseDTO.java
@@ -0,0 +1,15 @@
+package ar.com.mercadolibre.bootcam.calorias.dto;
+
+import ar.com.mercadolibre.bootcam.calorias.model.Ingrediente;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class PlatoResponseDTO {
+    private Integer cantidadTotalCalorias;
+    private List<Ingrediente> ingredientes;
+    private Ingrediente ingredienteMayorCalorias;
+}
---
+++ spring/calorias/src/test/java/ar/com/mercadolibre/bootcam/calorias/CaloriasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.mercadolibre.bootcam.calorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
