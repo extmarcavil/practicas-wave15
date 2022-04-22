f000d31aefa07c14dce18a2c2100e2491b4178cb
Alejandro Henestroza <alejandro.henestroza@mercadolibre.com>
2022-04-22 15:07:16 -0300

    Creado proyecto API Covid 19

---
+++ spring/api-covid-19/src/main/java/ar/com/alehenestroza/apicovid19/ApiCovid19Application.java
@@ -0,0 +1,13 @@
+package ar.com.alehenestroza.apicovid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCovid19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCovid19Application.class, args);
+    }
+
+}
---
+++ spring/api-covid-19/src/test/java/ar/com/alehenestroza/apicovid19/ApiCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.alehenestroza.apicovid19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCovid19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
