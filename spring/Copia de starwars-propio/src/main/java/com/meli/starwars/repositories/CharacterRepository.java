c13c9874f635a4a3cf0474937de91a0e3694fb84
Alan Gimenez <alan.gimenez@mercadolibre.com>
2022-04-24 18:22:45 -0300

    eliminar star wars duplicado

--- spring/Copia de starwars-propio/src/main/java/com/meli/starwars/repositories/CharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meli.starwars.repositories;
-
-import com.meli.starwars.dto.CharacterDTO;
-
-import java.util.List;
-
-public interface CharacterRepository {
-    List<CharacterDTO> findAllByNameContains(String query);
-}
