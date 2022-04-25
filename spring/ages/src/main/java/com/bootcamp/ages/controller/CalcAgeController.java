e5b7bcff729294f7d26909baca9ed86ca485a9ce
Diana1501 <diana.mhurtadog@gmail.com>
2022-04-25 08:19:34 -0500

    Implementando DTO

--- spring/ages/src/main/java/com/bootcamp/ages/controller/CalcAgeController.java
+++ spring/ages/src/main/java/com/bootcamp/ages/controller/CalcAgeController.java
@@ -1,6 +1,10 @@
 package com.bootcamp.ages.controller;
 
+import com.bootcamp.ages.dto.AgeDTO;
+import com.bootcamp.ages.dto.BornDTO;
 import org.apache.tomcat.jni.Local;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -10,11 +14,18 @@ import java.time.Period;
 @RequestMapping("/age")
 public class CalcAgeController {
 
-    @GetMapping("/calc/{day}/{month}")
-    public String calcAgePath(@PathVariable Integer day, @PathVariable Integer month, @RequestParam Integer year){
-        LocalDate born = LocalDate.of(year, month, day);
+    @GetMapping("/calc")
+    public ResponseEntity<AgeDTO> calcAgePath(@RequestBody BornDTO born){
         LocalDate now = LocalDate.now();
-        Period period = Period.between(born, now);
-        return "age: " + period.getYears();
+        Period period = Period.between(born.getBorn(), now);
+        return new ResponseEntity<>(new AgeDTO(period.getYears()), HttpStatus.I_AM_A_TEAPOT);
     }
+
+//    @GetMapping("/calc/{day}/{month}")
+//    public String calcAgePath(@PathVariable Integer day, @PathVariable Integer month, @RequestParam Integer year){
+//        LocalDate born = LocalDate.of(year, month, day);
+//        LocalDate now = LocalDate.now();
+//        Period period = Period.between(born, now);
+//        return "age: " + period.getYears();
+//    }
 }
