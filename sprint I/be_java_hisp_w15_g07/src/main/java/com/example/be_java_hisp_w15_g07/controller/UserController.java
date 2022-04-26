5e079159b67c967a828433932d0d63e2790c9999
Tomas Ravelli <tomas.ravelli@mercadolibre.com>
2022-04-26 16:27:04 -0300

    story 1 to test

--- sprint I/be_java_hisp_w15_g07/src/main/java/com/example/be_java_hisp_w15_g07/controller/UserController.java
+++ sprint I/be_java_hisp_w15_g07/src/main/java/com/example/be_java_hisp_w15_g07/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.example.be_java_hisp_w15_g07.controller;
 
 import com.example.be_java_hisp_w15_g07.service.IUserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -12,5 +13,12 @@ public class UserController {
 
     public UserController(IUserService userService){
         this.userService = userService;
+    }
+
+    @PostMapping("/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<?> followUser(@PathVariable Integer userId,
+                                     @PathVariable Integer userIdToFollow){
+        userService.followUser(userId, userIdToFollow);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- sprint I/be_java_hisp_w15_g07/src/main/java/com/example/be_java_hisp_w15_g07/repository/IUserRepository.java
+++ sprint I/be_java_hisp_w15_g07/src/main/java/com/example/be_java_hisp_w15_g07/repository/IUserRepository.java
@@ -1,4 +1,7 @@
 package com.example.be_java_hisp_w15_g07.repository;
 
+import com.example.be_java_hisp_w15_g07.model.User;
+
 public interface IUserRepository {
+    User findById(Integer id);
 }
