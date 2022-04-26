52fec88397e32d35e8b8f5f19ef857c55f974e89
gualdronsito <andres.gualdron@mercadolibre.com.co>
2022-04-26 09:49:06 -0500

    update

--- sprint II/calculadoraCalorias/src/main/java/com/meli/convert/repository/FoodRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meli.convert.repository;
-
-import com.meli.convert.model.Food;
-
-public interface FoodRepository {
-    Food getFodd(String name);
-}
--- sprint II/calculadoraCalorias/src/main/java/com/meli/convert/repository/IngredientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.meli.convert.repository;
-
-import com.meli.convert.model.Ingredient;
-
-public interface IngredientRepository {
-
-    Ingredient getIngredient(String name);
-}
