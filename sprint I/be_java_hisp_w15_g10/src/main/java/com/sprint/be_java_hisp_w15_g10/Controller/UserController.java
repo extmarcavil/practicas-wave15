2fec955e660b83ba04e9e92037b046359ccb3222
luisrodrigmeli <luis.irodriguez@mercadolibre.com.mx>
2022-04-26 12:28:07 -0500

    agregar controlador de Usuarios

---
+++ sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
@@ -0,0 +1,12 @@
+package com.sprint.be_java_hisp_w15_g10.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("users/")
+@RestController
+public class UserController {
+
+    
+
+}
