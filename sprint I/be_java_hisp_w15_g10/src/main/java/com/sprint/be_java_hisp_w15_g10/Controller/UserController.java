20c008467f38ebbc3e59fe929da0b7e829a395a3
alejoaguilarg <alejandro.aguilar@mercadolibre.com.co>
2022-04-26 12:53:46 -0500

    commit solucion de conflicto menor en controller

--- sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
+++ sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
@@ -1,6 +1,6 @@
 package com.sprint.be_java_hisp_w15_g10.Controller;
 
-import com.sprint.be_java_hisp_w15_g10.DTO.Response.UserWithFollowersCountDTO;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
