53c62615629b67f54720f1d8834ef1dfc08cc254
luisrodrigmeli <luis.irodriguez@mercadolibre.com.mx>
2022-04-26 12:49:12 -0500

    agregar Servicio Usuarios

--- sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
+++ sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.sprint.be_java_hisp_w15_g10.Controller;
 
 import com.sprint.be_java_hisp_w15_g10.DTO.Response.UserWithFollowersCountDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
@@ -9,5 +10,10 @@ import org.springframework.web.servlet.function.EntityResponse;
 @RequestMapping("users/")
 @RestController
 public class UserController {
+
+    @GetMapping("{userId}/followers/count")
+    public EntityResponse<UserWithFollowersCountDTO> getUserWitFollowersCount(@PathVariable int userId){
+        return null;
+    }
 
 }
