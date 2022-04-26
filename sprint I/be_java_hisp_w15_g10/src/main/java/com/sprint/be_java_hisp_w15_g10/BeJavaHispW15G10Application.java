a42c6c7cda568ccc9df2bcabe99710a09df1dca6
Diana1501 <diana.mhurtadog@gmail.com>
2022-04-26 09:37:43 -0500

    Proyecto base

---
+++ sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/BeJavaHispW15G10Application.java
@@ -0,0 +1,13 @@
+package com.sprint.be_java_hisp_w15_g10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW15G10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaHispW15G10Application.class, args);
+	}
+
+}
