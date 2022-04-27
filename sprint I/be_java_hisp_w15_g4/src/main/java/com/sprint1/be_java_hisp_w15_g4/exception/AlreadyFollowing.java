0f4a4f9e0b3f09eef4a75c2b87ad77e2086368f8
axsosa <axel.sosa@mercadolibre.com>
2022-04-27 12:26:39 -0300

    subo proyecto terminado y copia individual

---
+++ sprint I/be_java_hisp_w15_g4/src/main/java/com/sprint1/be_java_hisp_w15_g4/exception/AlreadyFollowing.java
@@ -0,0 +1,7 @@
+package com.sprint1.be_java_hisp_w15_g4.exception;
+
+public class AlreadyFollowing extends RuntimeException{
+    public AlreadyFollowing(Integer userId,Integer userIdToFollow) {
+        super(String.format("El usuario %d ya sigue al usuario %d",userId,userIdToFollow));
+    }
+}
