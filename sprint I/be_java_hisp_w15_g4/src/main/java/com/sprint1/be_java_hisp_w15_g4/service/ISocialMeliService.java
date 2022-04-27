3ea51374f7fd4c341ee71227198be7a9eee9a9c3
Lucas Jimenez <lucas.jimenez@mercadolibre.com>
2022-04-27 12:39:32 -0300

    Parte Grupal

---
+++ sprint I/be_java_hisp_w15_g4/src/main/java/com/sprint1/be_java_hisp_w15_g4/service/ISocialMeliService.java
@@ -0,0 +1,17 @@
+package com.sprint1.be_java_hisp_w15_g4.service;
+
+import com.sprint1.be_java_hisp_w15_g4.dto.request.PostDTO;
+import com.sprint1.be_java_hisp_w15_g4.dto.response.FollowerCountDTO;
+import com.sprint1.be_java_hisp_w15_g4.dto.response.FollowerListDTO;
+import com.sprint1.be_java_hisp_w15_g4.dto.response.FollowingListDTO;
+import com.sprint1.be_java_hisp_w15_g4.dto.response.PostListDTO;
+
+public interface ISocialMeliService {
+    void follow(int userID, int userIDToFollow); //Req 01
+    FollowerCountDTO countFollowers(int userID); //Req 02: devuelve la cantidad de usuarios que siguen a un vendedor dentro de un dto
+    FollowerListDTO listFollowers(int userID,String order); //Req 03: devuelve la lista de usuarios que siguen a un determinado usuario
+    FollowingListDTO listFollowing(int userID, String order); //Req 04: devuelve la lista de usuarios a quien sigue un determinado usuario
+    void createPost(PostDTO post); //Req 05: le llega una publicaciondto y devuelve un OK
+    PostListDTO lastTwoWeeksPosts(int userID, String order); //Req 06: Devuelve una lista con los post de las últimas dos semanas de los seguidos de un usuario.
+    void unfollow(int userID, int userIDToUnfollow); //Req 07
+}
