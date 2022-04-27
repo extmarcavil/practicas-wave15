8a878bc34311027cc6db09a71b50fd2d6757c389
gualdronsito <andres.gualdron@mercadolibre.com.co>
2022-04-27 09:25:23 -0500

    update 004 user history

--- sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
+++ sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.sprint.be_java_hisp_w15_g10.Controller;
 
 
+import com.sprint.be_java_hisp_w15_g10.DTO.Response.UserWithFollowersCountDTO;
+import com.sprint.be_java_hisp_w15_g10.DTO.Response.VendedorsFollowedDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,11 @@ public class UserController {
 
     @GetMapping("{userId}/followers/count")
     public EntityResponse<UserWithFollowersCountDTO> getUserWitFollowersCount(@PathVariable int userId){
+        return null;
+    }
+
+    @GetMapping("/{userId}/followed/list")
+    public EntityResponse<VendedorsFollowedDTO> getVendorsFollow(@PathVariable int userId) {
         return null;
     }
 
---
+++ sprint I/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/DTO/Response/VendedorsFollowedDTO.java
@@ -0,0 +1,4 @@
+package com.sprint.be_java_hisp_w15_g10.DTO.Response;
+
+public class VendedorsFollowedDTO {
+}
