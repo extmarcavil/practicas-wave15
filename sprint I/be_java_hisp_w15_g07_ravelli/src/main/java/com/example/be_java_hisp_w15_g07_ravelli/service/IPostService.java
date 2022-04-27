9ae5e9bcf1fd9d821e8b0d1780ce881ce0f5689c
Tomas Ravelli <tomas.ravelli@mercadolibre.com>
2022-04-27 20:18:15 -0300

    initial personal project

---
+++ sprint I/be_java_hisp_w15_g07_ravelli/src/main/java/com/example/be_java_hisp_w15_g07_ravelli/service/IPostService.java
@@ -0,0 +1,13 @@
+package com.example.be_java_hisp_w15_g07_ravelli.service;
+
+
+import com.example.be_java_hisp_w15_g07_ravelli.dto.request.NewPostDTO;
+import com.example.be_java_hisp_w15_g07_ravelli.dto.response.UserFollowedPostsDTO;
+
+public interface IPostService {
+
+    void newPost(NewPostDTO postDTO);
+
+    UserFollowedPostsDTO getFollowedPosts(Integer userId, String order);
+
+}
