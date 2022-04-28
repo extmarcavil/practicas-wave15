56ec2f1e1c5f0a334c321cb54bf9caac8ae533ca
Gomez Rodrigo <rodrigogabriel.gomez@mercadolibre.com>
2022-04-28 10:38:06 -0300

    individual

---
+++ sprint I/be_java_hisp_w15_g05_gomez/src/main/java/com/example/be_java_hisp_w15_g05/service/IProductsService.java
@@ -0,0 +1,13 @@
+package com.example.be_java_hisp_w15_g05.service;
+
+import com.example.be_java_hisp_w15_g05.dto.PostDTO;
+import com.example.be_java_hisp_w15_g05.dto.ResCreatePostDTO;
+import com.example.be_java_hisp_w15_g05.dto.ResPostListDTO;
+
+import java.util.List;
+
+public interface IProductsService {
+
+    ResCreatePostDTO createPost(PostDTO postDTO);
+    ResPostListDTO getPostFollowed(int id, String order);
+}
