1978814bf10d2784ed6c0ee08a21f828f0ec2301
na-garcia <nicolasariel.garcia@mercadolibre.com>
2022-04-28 15:13:05 -0300

    Sprint I parte grupal

---
+++ sprint I/be_java_hisp_w15_g4_grupal/src/test/java/com/sprint1/be_java_hisp_w15_g4/BeJavaHispW15G4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint1.be_java_hisp_w15_g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW15G4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
