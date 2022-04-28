dbde4d8a9b632a333f778f22a87ec24cc16b229d
AnaSalomone <ana.salomone@mercadolibre.com>
2022-04-28 17:07:16 -0300

    agregando proyecto grupal

---
+++ sprint I/grupal/be_java_hisp_w15_g01/src/main/java/ar/com/mercadolibre/bootcamp/be_java_hisp_w15_g01/service/PostService.java
@@ -0,0 +1,15 @@
+package ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.service;
+
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.dto.PostDTO;
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.dto.PostListDTO;
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.dto.ResponseDTO;
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.model.Product;
+
+import java.time.LocalDate;
+
+public interface PostService {
+
+    ResponseDTO createPost(PostDTO postDto);
+
+    PostListDTO getPostsByFollowedUsers(Long userId, String order);
+}
