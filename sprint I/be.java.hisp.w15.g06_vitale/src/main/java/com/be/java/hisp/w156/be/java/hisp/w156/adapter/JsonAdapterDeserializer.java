d47369ee344888039bd535edcf50bae3b3db68ac
NicoVitale <nicolas.vitale@mercadolibre.com>
2022-04-28 10:38:18 -0300

    Se sube el proyecto grupal y la primera versión del proyecto individual

---
+++ sprint I/be.java.hisp.w15.g06_vitale/src/main/java/com/be/java/hisp/w156/be/java/hisp/w156/adapter/JsonAdapterDeserializer.java
@@ -0,0 +1,22 @@
+package com.be.java.hisp.w156.be.java.hisp.w156.adapter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@JsonComponent
+public class JsonAdapterDeserializer extends JsonDeserializer<LocalDate> {
+
+    private final DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    @Override
+    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        return LocalDate.parse(jsonParser.getValueAsString(), formatter);
+    }
+}
