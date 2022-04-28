5a0d230c9dc7bc89c7ef63ca63f7a255c3331f18
gonzalosinnottsegura <gonzalo.sinnott@mercadolibre.com>
2022-04-28 08:45:35 -0300

    Merge Proyecto grupal a mi rama

---
+++ spring/be_java_hisp_w15_g01/src/main/java/com/example/be_java_hisp_w15_g01/BeJavaHispW15G01Application.java
@@ -0,0 +1,13 @@
+package com.example.be_java_hisp_w15_g01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW15G01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJavaHispW15G01Application.class, args);
+    }
+
+}
---
+++ sprint I/be_java_hisp_w15_g01_sinnott/src/main/java/ar/com/mercadolibre/bootcamp/be_java_hisp_w15_g01/repository/PostRepositoryImpl.java
@@ -0,0 +1,51 @@
+package ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.repository;
+
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.model.Post;
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.model.Product;
+import ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+
+    List<Post> posts = new ArrayList<>();
+
+    @Override
+    public Post create(User user, LocalDate date, Product detail, Integer category, Float price) {
+
+        Post post = new Post();
+        post.setPostId(posts.size() +1L);
+        post.setUser(user);
+        post.setDate(date);
+        post.setDetail(detail);
+        post.setCategory(category);
+        post.setPrice(price);
+
+        posts.add(post);
+
+        return post;
+    }
+    
+    @Override
+    public boolean isseller(User user) {
+        return this.posts.stream().anyMatch(p -> p.getUser().equals(user));
+    }
+
+    @Override
+    public List<Post> getAllPostsByUserWithinTimespan(User user, int daysBack) {
+        LocalDate now = LocalDate.now();
+
+        List<Post> postList = posts.stream()
+                .filter( p -> p.getUser().getUserId().equals(user.getUserId()) &&
+                        p.getDate().compareTo(now.minusDays(daysBack)) > 0)
+                .collect(Collectors.toList());
+
+        return postList;
+    }
+
+}
