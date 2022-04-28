6a9162038d86233c470c0a67e66258f77b46dcb2
mgomezML <mauricio.gomez@mercadolibre.com>
2022-04-28 09:17:31 -0300

    Brought sprint I project

---
+++ sprint I/be_java_hisp_w15_g07/src/main/java/com/example/be_java_hisp_w15_g07/controller/PostController.java
@@ -0,0 +1,39 @@
+package com.example.be_java_hisp_w15_g07.controller;
+
+
+import com.example.be_java_hisp_w15_g07.dto.response.UserFollowedPostsDTO;
+import com.example.be_java_hisp_w15_g07.service.IPostService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.be_java_hisp_w15_g07.dto.request.NewPostDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/products")
+public class PostController {
+
+    private IPostService postService;
+
+    public PostController(IPostService postService){
+        this.postService = postService;
+    }
+
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<UserFollowedPostsDTO> getUserFollowedPosts (@PathVariable Integer userId, @RequestParam(defaultValue = "date_asc") String order) {
+        return new ResponseEntity<>(postService.getFollowedPosts(userId, order), HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> newPost(@RequestBody NewPostDTO postDTO){
+        postService.newPost(postDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+}
