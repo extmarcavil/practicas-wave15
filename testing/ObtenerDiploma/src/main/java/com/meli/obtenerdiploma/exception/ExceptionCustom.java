fad285de66069897e96c41c8a2590a47b2c8a7ee
gpichetti <gonzalo.pichetti@mercadolibre.com>
2022-04-29 17:50:56 -0300

    Ejercicio de Testing ObtenerDiploma

---
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/ExceptionCustom.java
@@ -0,0 +1,7 @@
+package com.meli.obtenerdiploma.exception;
+
+public class ExceptionCustom extends RuntimeException{
+    public ExceptionCustom(String message){
+        super(message);
+    }
+}
---
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,24 @@
+package com.meli.obtenerdiploma.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+
+    @NotEmpty(message = "El nombre del alumno no puede estar vacio.")
+    @Size(max = 50,message = "La longitud del nombre no puede superar los 50 caracteres.")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía.")
+
+    List<@Valid SubjectDTO> subjects;
+}
