9c8a0c7f61df4028011bf0f40b34289c09bc5b4e
IFerrante-meli <ignacio.ferrante@mercadolibre.com>
2022-04-29 16:56:31 -0300

    no valida las materias

--- testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.List;
 
