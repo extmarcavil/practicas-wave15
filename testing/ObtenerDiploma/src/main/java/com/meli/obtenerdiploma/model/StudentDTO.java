50a307f8628d5fca91399175464c5ffa104b748d
lucho-barrera <luciano.barrera@mercadolibre.com>
2022-04-29 17:50:29 -0300

    bugs fix testing

--- testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -5,20 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
-    @NotEmpty(message = "El nombre del alumno no puede estar vacío")
+    @NotNull(message = "El nombre del alumno no puede estar vacío")
     @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
-    @Pattern(regexp = "[A-Z][a-zA-Z]+", message = "El nombre del alumno debe comenzar con mayúscula.")
+    @Pattern(regexp = "[A-Z].*", message = "El nombre del alumno debe comenzar con mayúscula.")
     String studentName;
     String message;
     Double averageScore;
     @NotEmpty(message = "La lista no puede ser vacía.")
+    @Valid
     List<SubjectDTO> subjects;
 }
--- testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -8,11 +8,11 @@ import javax.validation.constraints.*;
 
 @Getter @Setter
 public class SubjectDTO {
-    @NotEmpty(message = "El nombre de la materia no puede estar vacío.")
+    @NotNull(message = "El nombre de la materia no puede estar vacío.")
     @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
-    @Pattern(regexp = "/^[A-Z][a-z0-9_-]$/", message = "El nombre de la materia debe comenzar con mayúscula.")
+    @Pattern(regexp = "[A-Z].*", message = "El nombre de la materia debe comenzar con mayúscula.")
      String name;
-    @NotEmpty(message = "La nota no puede estar vacía.")
+    @NotNull(message = "La nota no puede estar vacía.")
     @Min(value = 0, message = "la minima nota es 0.0")
     @Max(value = 10,message = "La máxima nota es 10.0")
     Double score;
