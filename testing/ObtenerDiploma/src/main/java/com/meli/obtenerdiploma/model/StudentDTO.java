ab1905a1aec3684e3d658e1a43af148756bde5ce
lmacbit <lorena.maciel@mercadolibre.com>
2022-05-02 13:13:21 -0300

    Ejercicio Validaciones de Introducción a la calidad SW P1 PG

---
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,23 @@
+package com.meli.obtenerdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+    @NotNull(message = "El nombre del alumno no puede estar vacío.")
+    @Pattern(regexp = "^[A-Z].*", message = "El nombre del alumno debe comenzar con mayúscula.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía.")
+    List<@Valid SubjectDTO> subjects;
+}
---
+++ testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -0,0 +1,18 @@
+package com.meli.obtenerdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+
+@Getter @Setter
+public class SubjectDTO {
+    @NotNull(message = "El nombre de la materia no puede estar vacío.")
+    @Pattern(regexp = "^[A-Z].*", message = "El nombre de la materia debe comenzar con mayúscula.")
+    @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
+    String name;
+    @NotNull(message = "La nota no puede estar vacía.")
+    @Min(value = 0 , message = "La mínima nota es 0.0.")
+    @Max(value = 10, message = "La máxima nota es 10.0.")
+    Double score;
+}
