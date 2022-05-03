c17c480dcfe3bc4bbfec60a9641de9c5cfef4bb7
Gomez Rodrigo <rodrigogabriel.gomez@mercadolibre.com>
2022-05-03 11:40:28 -0300

    Obtener diploma Test

---
+++ testing/ObtenerDiploma 2/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
@@ -0,0 +1,21 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.service.IObtenerDiplomaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class ObtenerDiplomaController {
+
+    @Autowired
+    IObtenerDiplomaService service;
+
+    @GetMapping("/analyzeScores/{studentId}")
+    public StudentDTO analyzeScores(@PathVariable Long studentId) {
+        return service.analyzeScores(studentId);
+    }
+}
