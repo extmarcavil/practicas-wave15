e5e76e0e78a7aef682b77953b2b6d6862de3a224
Lucas Jimenez <lucas.jimenez@mercadolibre.com>
2022-05-03 19:44:15 -0300

    Unit Test Controller

--- testing/Ejercicios Practicos/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/unit/controller/ObtenerDiplomaControllerTest.java
+++ testing/Ejercicios Practicos/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/unit/controller/ObtenerDiplomaControllerTest.java
@@ -14,6 +14,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Set;
+
 @ExtendWith(MockitoExtension.class)
 public class ObtenerDiplomaControllerTest {
 
@@ -43,6 +45,44 @@ public class ObtenerDiplomaControllerTest {
         ResponseEntity<?> response=controller.registerStudent(student);
         Assertions.assertAll(
                 ()->Mockito.verify(service,Mockito.times(1)).create(student),
+                ()->Assertions.assertEquals(HttpStatus.OK,response.getStatusCode()),
+                ()->Assertions.assertNull(response.getBody())
+        );
+    }
+
+    @Test
+    void removeStudentOk(){
+        ResponseEntity<?> response=controller.removeStudent(1L);
+        Assertions.assertAll(
+                ()->Mockito.verify(service,Mockito.times(1)).delete(1L),
+                ()->Assertions.assertEquals(HttpStatus.OK,response.getStatusCode()),
+                ()->Assertions.assertNull(response.getBody())
+        );
+    }
+
+    @Test
+    void listStudentOk(){
+        Set<StudentDTO> mock=Set.of(
+                StudentFactory.getJuan(),
+                StudentFactory.getPedro()
+        );
+        Mockito.when(service.getAll()).thenReturn(mock);
+        Set<StudentDTO> response=controller.listStudents();
+        Assertions.assertAll(
+                ()->Mockito.verify(service,Mockito.times(1)).getAll(),
+                ()->Assertions.assertEquals(2,response.size()),
+                ()->Assertions.assertNotNull(response)
+        );
+    }
+
+    @Test
+    void modifyStudentOk(){
+        StudentDTO student= StudentFactory.getJuan();
+        ResponseEntity<?> response=controller.modifyStudent(student);
+        Assertions.assertAll(
+                ()->Mockito.verify(service,Mockito.times(1)).update(student),
+                ()->Mockito.verify(service,Mockito.times(0)).create(student),
+                ()->Mockito.verify(service,Mockito.times(0)).delete(student.getId()),
                 ()->Assertions.assertEquals(HttpStatus.OK,response.getStatusCode()),
                 ()->Assertions.assertNull(response.getBody())
         );
