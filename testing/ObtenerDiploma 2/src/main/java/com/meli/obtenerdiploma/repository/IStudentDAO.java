6ea765a0f96b05e96314243ab71e87125dcd678c
gesanchez92 <gerson.sanchez@mercadolibre.com.mx>
2022-05-03 07:17:46 -0500

    test unitario sin mocks, ejercicio 1

---
+++ testing/ObtenerDiploma 2/src/main/java/com/meli/obtenerdiploma/repository/IStudentDAO.java
@@ -0,0 +1,14 @@
+package com.meli.obtenerdiploma.repository;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+
+import java.util.List;
+import java.util.Set;
+
+public interface IStudentDAO {
+    void save(StudentDTO stu);
+    boolean delete(Long id);
+    boolean exists(StudentDTO stu);
+    StudentDTO findById(Long id);
+    List<StudentDTO> allStudents();
+}
