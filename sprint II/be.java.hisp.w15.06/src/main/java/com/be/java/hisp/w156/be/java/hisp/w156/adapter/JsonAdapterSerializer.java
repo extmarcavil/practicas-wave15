ce95510c9475b70d3e40c52c94c2ac654ef9a78b
malvarez22 <matiassebastian.alvarez@mercadolibre.com>
2022-05-04 15:11:11 -0300

    Sprint 2 - primer commit con el proyecto grupal base

---
+++ sprint II/be.java.hisp.w15.06/src/main/java/com/be/java/hisp/w156/be/java/hisp/w156/adapter/JsonAdapterSerializer.java
@@ -0,0 +1,21 @@
+package com.be.java.hisp.w156.be.java.hisp.w156.adapter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@JsonComponent
+public class JsonAdapterSerializer extends JsonSerializer<LocalDate> {
+
+    private final DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    @Override
+    public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(formatter.format(localDate));
+    }
+}
