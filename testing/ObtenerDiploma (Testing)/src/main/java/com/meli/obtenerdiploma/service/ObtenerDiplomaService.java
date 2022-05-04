ce8bd9820c654bd7679f17d389b8e47bb798b5f0
Ramiro Vera <ramiro.vera@mercadolibre.com>
2022-05-04 13:11:01 -0300

    Change name of Obtener Diploma Exercise

---
+++ testing/ObtenerDiploma (Testing)/src/main/java/com/meli/obtenerdiploma/service/ObtenerDiplomaService.java
@@ -0,0 +1,38 @@
+package com.meli.obtenerdiploma.service;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import com.meli.obtenerdiploma.repository.IStudentDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+@Service
+public class ObtenerDiplomaService implements IObtenerDiplomaService {
+
+    @Autowired
+    IStudentDAO studentDAO;
+
+    @Override
+    public StudentDTO analyzeScores(Long studentId) {
+        StudentDTO stu = studentDAO.findById(studentId);
+
+        stu.setAverageScore(calculateAverage(stu.getSubjects()));
+        stu.setMessage(getGreetingMessage(stu.getStudentName(), stu.getAverageScore()));
+
+        return stu;
+    }
+
+    private String getGreetingMessage(String studentName, Double average) {
+        return "El alumno " + studentName + " ha obtenido un promedio de " + new DecimalFormat("#.##").format(average)
+                + ((average > 9) ? ". Felicitaciones!" : ". Puedes mejorar.");
+    }
+
+    private Double calculateAverage(List<SubjectDTO> scores) {
+        return scores.stream()
+                .reduce(0D, (partialSum, score)  -> partialSum + score.getScore(), Double::sum)
+                / scores.size();
+    }
+}
---
+++ testing/ObtenerDiploma (Testing)/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.meli.obtenerdiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObetenerDiplomaApplicationTests {
+
+
+
+	@Test
+	public void contextLoads() {
+
+	}
+}
