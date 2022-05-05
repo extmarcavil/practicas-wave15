fa3cbb6035d9e73b7ec1f41dea8f4c41e0edbb39
kopitz <klaus.opitz@mermercadolibre.com.mx>
2022-05-05 08:40:18 -0500

    archivo de prueba

---
+++ testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaControllerTest.java
@@ -0,0 +1,12 @@
+package com.meli.obtenerdiploma.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ObtenerDiplomaControllerTest {
+
+    @Test
+    public void analyzeScoresTest() {
+        assertTrue(true);
+    }
+}
