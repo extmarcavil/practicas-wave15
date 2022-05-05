a9279bd813390f2761fa03cfcb383b0bcbfe69fd
Maria Belen Tracchia <mariabelen.tracchia@mercadolibre.com>
2022-05-05 15:11:14 -0300

    Sprint II - grupal

---
+++ sprint II/be_java_hisp_w15_g03/src/main/java/com/sprint1/be_java_hisp_w15_g03/dto/response/SellerPListDTO.java
@@ -0,0 +1,22 @@
+package com.sprint1.be_java_hisp_w15_g03.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * US:06
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SellerPListDTO {
+
+    private Integer userId;
+    private List<PublicationRespDTO> posts;
+}
