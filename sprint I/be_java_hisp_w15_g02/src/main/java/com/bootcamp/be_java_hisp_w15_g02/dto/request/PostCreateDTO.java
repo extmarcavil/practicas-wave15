92c1362dfbb8a127e6fa5d0ffa23fe05f42d28fa
javilamip <jose.villamizar@mercadolibre.com.co>
2022-05-05 13:03:21 -0400

    Added group work for sprint 1

---
+++ sprint I/be_java_hisp_w15_g02/src/main/java/com/bootcamp/be_java_hisp_w15_g02/dto/request/PostCreateDTO.java
@@ -0,0 +1,42 @@
+package com.bootcamp.be_java_hisp_w15_g02.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+public class PostCreateDTO {
+    private int user_id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    private ProductDTO detail;
+    private String category;
+    private double price;
+    private boolean has_Promo;
+    private double discount;
+
+    public PostCreateDTO() {
+    }
+
+    public PostCreateDTO(int user_id, LocalDate date, ProductDTO detail, String category, double price) {
+        this.user_id = user_id;
+        this.date = date;
+        this.detail = detail;
+        this.category = category;
+        this.price = price;
+    }
+
+    public PostCreateDTO(int user_id, LocalDate date, ProductDTO detail, String category, double price, boolean has_Promo, double discount) {
+        this.user_id = user_id;
+        this.date = date;
+        this.detail = detail;
+        this.category = category;
+        this.price = price;
+        this.has_Promo = has_Promo;
+        this.discount = discount;
+    }
+}
