8f1daaced254d95379c4fdbca8810574eab199c6
Gomez Rodrigo <rodrigogabriel.gomez@mercadolibre.com>
2022-05-05 10:37:11 -0300

    Estructura de carpetas

---
+++ sprint II/be_java_hisp_w15_g05/src/test/java/com/example/be_java_hisp_w15_g05/unit/service/FollowsServiceTest.java
@@ -0,0 +1,5 @@
+package com.example.be_java_hisp_w15_g05.unit.service;
+
+public class FollowsServiceTest {
+
+}
---
+++ sprint II/be_java_hisp_w15_g05/src/test/java/com/example/be_java_hisp_w15_g05/unit/service/ProductsServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.be_java_hisp_w15_g05.unit.service;
+
+public class ProductsServiceTest {
+}
