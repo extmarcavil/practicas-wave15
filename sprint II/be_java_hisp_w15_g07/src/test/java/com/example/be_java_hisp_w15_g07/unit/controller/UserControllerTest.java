24432a9e386be38ad5d9c38358d9ec89258e186e
mgomezML <mauricio.gomez@mercadolibre.com>
2022-05-05 10:31:36 -0300

    T0001 tests and structure added

---
+++ sprint II/be_java_hisp_w15_g07/src/test/java/com/example/be_java_hisp_w15_g07/unit/controller/UserControllerTest.java
@@ -0,0 +1,61 @@
+package com.example.be_java_hisp_w15_g07.unit.controller;
+
+import com.example.be_java_hisp_w15_g07.controller.UserController;
+import com.example.be_java_hisp_w15_g07.exception.UserNotFoundException;
+import com.example.be_java_hisp_w15_g07.service.IUserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+    @Mock
+    IUserService service;
+
+    @InjectMocks
+    UserController controller;
+
+    @Test
+    @DisplayName("T00001 - Verificar que el usuario a seguir exista")
+    public void findExistingUser(){
+        //arrange
+        Integer userId = 1;
+        Integer queryId = 2;
+        ResponseEntity<?> expected = new ResponseEntity(HttpStatus.OK);
+
+        //Mockito
+        doNothing().when(service).followUser(userId, queryId);
+
+        //act
+        ResponseEntity<?> result = controller.followUser(userId, queryId);
+
+        //assert
+        assertEquals(expected, result);
+        verify(service, times(1)).followUser(anyInt(), anyInt());
+    }
+
+    @Test
+    @DisplayName("T00001 - Verificar que si el usuario a seguir no existe, lanza excepción")
+    public void throwExceptionWhenUserNotFound(){
+        //arrange
+        Integer userId = 1;
+        Integer queryId = -2;
+
+        //Mockito
+        doThrow(UserNotFoundException.class).when(service).followUser(userId, queryId);
+
+        //act and assert
+        assertThrows(UserNotFoundException.class, () -> controller.followUser(userId, queryId));
+        verify(service, times(1)).followUser(anyInt(), anyInt());
+    }
+}
---
+++ sprint II/be_java_hisp_w15_g07/src/test/java/com/example/be_java_hisp_w15_g07/utils/UserFactory.java
@@ -0,0 +1,22 @@
+package com.example.be_java_hisp_w15_g07.utils;
+
+import com.example.be_java_hisp_w15_g07.model.Post;
+import com.example.be_java_hisp_w15_g07.model.Product;
+import com.example.be_java_hisp_w15_g07.model.User;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class UserFactory {
+    public static User getUserOne(){
+        return new User(1, "User 1");
+    }
+
+    public static User getUserTwo(){
+        User user2 = new User(2, "User 2");
+        Product product = new Product(1, "Televisor 43", "Electrodomesticos", "Samsung", "Negro", "");
+        Post post = new Post(LocalDate.of(2020, Month.APRIL, 10), product, 1, 60000d);
+        user2.newPost(post);
+        return user2;
+    }
+}
