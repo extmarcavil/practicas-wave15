29c4cd8ef6a87ee85e5b3858fc987d4f0d4e2668
axsosa <axel.sosa@mercadolibre.com>
2022-05-05 11:16:38 -0300

    Mejoro creacion usando el TestGenerator

--- sprint II/be_java_hisp_w15_g4/src/test/java/com/sprint1/be_java_hisp_w15_g4/unit/T0007.java
+++ sprint II/be_java_hisp_w15_g4/src/test/java/com/sprint1/be_java_hisp_w15_g4/unit/T0007.java
@@ -1,5 +1,6 @@
 package com.sprint1.be_java_hisp_w15_g4.unit;
 
+import Utils.TestGenerator;
 import com.sprint1.be_java_hisp_w15_g4.dto.response.FollowerCountDTO;
 import com.sprint1.be_java_hisp_w15_g4.exception.IDNotFoundException;
 import com.sprint1.be_java_hisp_w15_g4.model.User;
@@ -26,35 +27,26 @@ public class T0007 {
     @DisplayName("Verificar si el calculo de la cantidad de seguidores de un usuario es correcta.")
     void verificarCantidadSeguidoresOK() {
         // Arrange
-        User user1 = new User(1, "nombre1");
-        User user2 = new User(2, "nombre2");
-        User user3 = new User(3, "nombre3");
+        User user = TestGenerator.userWithFollowers();
 
-        /* Usuario 1 y 3 siguien al usuario 2 */
-        user1.addFollowing(user2);//user siguiendo al 2
-        user3.addFollowing(user2);//user3 siguiendo al 2
-        /* Usuario 2 es seguido por el usuario 1 y 3 */
-        user2.addFollower(user1);//user 2 lo sigue 1
-        user2.addFollower(user3);//user 2 lo sigue 3
-        /* Obtengo el DTO con los seguidores del Usuario 2 */
-        FollowerCountDTO seguidoresUser2=
-                new FollowerCountDTO(user2.getUser_id(),user2.getUser_name(),user2.getFollowers().size());
+        FollowerCountDTO seguidoresUser=
+                new FollowerCountDTO(user.getUser_id(),user.getUser_name(),user.getFollowers().size());
 
         // Mock
-        Mockito.when(repo.findUser(Mockito.anyInt())).thenReturn(user2);
+        Mockito.when(repo.findUser(Mockito.anyInt())).thenReturn(user);
 
         // Act
         /* Ejecuto el metodo para obtener el DTO del service */
-        FollowerCountDTO resultFolowers = service.countFollowers(user2.getUser_id());
+        FollowerCountDTO resultFolowers = service.countFollowers(user.getUser_id());
         // Assert
         /*
         * Verifico si la cantidad de seguidores del resultado es la misma que la creada
-        * Verifico que la cantidad de seguidores sea 2
+        * Verifico que la cantidad de seguidores sea 3
         * Verifico que la cantidad de seguidores sea distinto de 0
         * */
         Assertions.assertAll(
-                () -> Assertions.assertEquals(seguidoresUser2.getFollowers_count(),resultFolowers.getFollowers_count()),
-                () -> Assertions.assertEquals(2,resultFolowers.getFollowers_count()),
+                () -> Assertions.assertEquals(seguidoresUser.getFollowers_count(),resultFolowers.getFollowers_count()),
+                () -> Assertions.assertEquals(3,resultFolowers.getFollowers_count()),
                 () -> Assertions.assertTrue(resultFolowers.getFollowers_count()!=0)
         );
     }
