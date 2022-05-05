8197f06d078a5614367b73a6e5b02472979d4546
kopitz <klaus.opitz@mermercadolibre.com.mx>
2022-05-05 04:27:35 -0500

    little test

--- testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
+++ testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/ObtenerDiplomaApplicationTests.java
@@ -1,10 +1,17 @@
 package com.meli.obtenerdiploma;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ObetenerDiplomaApplicationTests {
+
+	@Autowired
+	public MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
