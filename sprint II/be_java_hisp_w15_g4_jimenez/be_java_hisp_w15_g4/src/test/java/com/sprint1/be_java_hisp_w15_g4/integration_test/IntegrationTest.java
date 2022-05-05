e9d5056d163b91c066affbf27614a2289c321a8d
Lucas Jimenez <lucas.jimenez@mercadolibre.com>
2022-05-05 15:51:20 -0300

    Test de integracion y coverage 84%

---
+++ sprint II/be_java_hisp_w15_g4_jimenez/be_java_hisp_w15_g4/src/test/java/com/sprint1/be_java_hisp_w15_g4/integration_test/IntegrationTest.java
@@ -0,0 +1,101 @@
+package com.sprint1.be_java_hisp_w15_g4.integration_test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.sprint1.be_java_hisp_w15_g4.dto.response.ErrorDTO;
+import com.sprint1.be_java_hisp_w15_g4.dto.response.IdNotPositiveErrorDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Map;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void followUseOk() throws Exception {
+
+        int idUser=1;
+        int idUserToFollow=2;
+
+        ResultMatcher status= MockMvcResultMatchers.status().isOk();
+
+        MockHttpServletRequestBuilder req = MockMvcRequestBuilders.post("/users/{user_id}/follow/{userIdToFollow}",idUser,idUserToFollow);
+
+        mockMvc.perform(req)
+                .andExpectAll(
+                        status
+                );
+    }
+
+    @Test
+    void followUseNotExists() throws Exception {
+
+        ObjectWriter writer= new ObjectMapper().writer();
+
+        int idUser=1;
+        int idUserToFollow=7;
+
+        ErrorDTO expected= new ErrorDTO("No se ha encontrado el usuario: 7", HttpStatus.NOT_FOUND);
+        String jsonString= writer.writeValueAsString(expected);
+
+        ResultMatcher status= MockMvcResultMatchers.status().isNotFound();
+        ResultMatcher json=MockMvcResultMatchers.content().json(jsonString);
+        ResultMatcher contentType=MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+        MockHttpServletRequestBuilder req = MockMvcRequestBuilders.post("/users/{user_id}/follow/{userIdToFollow}",idUser,idUserToFollow);
+
+        mockMvc.perform(req)
+                .andExpectAll(
+                        status,
+                        json,
+                        contentType
+                );
+    }
+
+
+    @Test
+    void followUseBadUserId() throws Exception {
+
+        ObjectWriter writer= new ObjectMapper().writer();
+
+        int idUser=-1;
+        int idUserToFollow=-7;
+
+        IdNotPositiveErrorDTO expected= new IdNotPositiveErrorDTO(
+                HttpStatus.BAD_REQUEST,
+                Map.of("userId","El id debe ser mayor a cero.",
+                        "userIdToFollow","El id debe ser mayor a cero.")
+        );
+        String jsonString= writer.writeValueAsString(expected);
+
+        ResultMatcher status= MockMvcResultMatchers.status().isBadRequest();
+        ResultMatcher json=MockMvcResultMatchers.content().json(jsonString);
+        ResultMatcher contentType=MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+        MockHttpServletRequestBuilder req = MockMvcRequestBuilders.post("/users/{user_id}/follow/{userIdToFollow}",idUser,idUserToFollow);
+
+        mockMvc.perform(req)
+                .andExpectAll(
+                        status,
+                        json,
+                        contentType
+                );
+    }
+
+}
