3d7223aeed26bab636a4cd9dad902f8fbcba9d19
emarichal <esteban.marichal@mercadolibre.com>
2022-05-06 10:19:59 -0300

    Test integration para ProductController y mayor coverage.

--- sprint II/be_java_hisp_w15_g03_Marichal_Esteban/src/test/java/com/sprint1/be_java_hisp_w15_g03/integration/ProductControllerIT.java
+++ sprint II/be_java_hisp_w15_g03_Marichal_Esteban/src/test/java/com/sprint1/be_java_hisp_w15_g03/integration/ProductControllerIT.java
@@ -1,23 +1,33 @@
 package com.sprint1.be_java_hisp_w15_g03.integration;
 
-import com.sprint1.be_java_hisp_w15_g03.model.Publication;
-import com.sprint1.be_java_hisp_w15_g03.model.Seller;
-import com.sprint1.be_java_hisp_w15_g03.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sprint1.be_java_hisp_w15_g03.dto.ProductDTO;
+import com.sprint1.be_java_hisp_w15_g03.dto.request.PublicationDTO;
+import com.sprint1.be_java_hisp_w15_g03.dto.response.SellerCountDTO;
+import com.sprint1.be_java_hisp_w15_g03.dto.response.SellerPListDTO;
+import com.sprint1.be_java_hisp_w15_g03.exception.CategoryNotFoundException;
+import com.sprint1.be_java_hisp_w15_g03.exception.OrderInvalidException;
+import com.sprint1.be_java_hisp_w15_g03.exception.PersonNotFoundException;
 import com.sprint1.be_java_hisp_w15_g03.repository.MeliRepository;
+import com.sprint1.be_java_hisp_w15_g03.utils.TestUtilsGenerator;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDate;
 
 
 @SpringBootTest
@@ -30,5 +40,232 @@ public class ProductControllerIT {
     @Autowired
     MeliRepository meliRepository;
 
+    @BeforeEach
+    void setup() {
+        meliRepository.loadDataTest(TestUtilsGenerator.dataToTest());
+    }
+
+    @AfterEach
+    void after() {
+        meliRepository.removeDataTest(TestUtilsGenerator.dataToTest());
+    }
+
+
+    @Test
+    @DisplayName("Verificar crear una publicación correctamente.")
+    void savePublicationOk() throws Exception {
+        ObjectWriter writer = new ObjectMapper()
+                .registerModule(new JavaTimeModule()) // convertir fechas
+                .writer();
+
+        // Arrange
+        ProductDTO productDTO = new ProductDTO(1, "Mesa", "Roble", "Buena", "Verde", "con detalles");
+        PublicationDTO publicationDTO = new PublicationDTO(1, LocalDate.now(), productDTO, 2, 300.0);
+
+        String payload = writer.writeValueAsString(publicationDTO);
+
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(expectedStatus);
+    }
+
+    @Test
+    @DisplayName("Verificar crear una publicación con fallo en las validaciones.")
+    void savePublicationMethodArgumentNotValidException() throws Exception {
+        ObjectWriter writer = new ObjectMapper()
+                .registerModule(new JavaTimeModule()) // convertir fechas
+                .writer();
+
+        // Arrange
+        ProductDTO productDTO = new ProductDTO(1, "Mesa", null, "Buena", "Verde", "con detalles");
+        PublicationDTO publicationDTO = new PublicationDTO(-1, LocalDate.now(), productDTO, 2, 300.0);
+
+        String payload = writer.writeValueAsString(publicationDTO);
+
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isBadRequest();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        result -> Assertions.assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException)
+                 );
+    }
+
+    @Test
+    @DisplayName("Verificar crear una publicación con una categoría inexistente.")
+    void savePublicationCategoryNotFoundException() throws Exception {
+        ObjectWriter writer = new ObjectMapper()
+                .registerModule(new JavaTimeModule()) // convertir fechas
+                .writer();
+
+        // Arrange
+        ProductDTO productDTO = new ProductDTO(1, "Mesa", "Roble", "Buena", "Verde", "con detalles");
+        PublicationDTO publicationDTO = new PublicationDTO(1, LocalDate.now(), productDTO, 100, 300.0);
+
+        String payload = writer.writeValueAsString(publicationDTO);
+
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isNotFound();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        result -> Assertions.assertTrue(result.getResolvedException() instanceof CategoryNotFoundException)
+                );
+    }
+
+    @Test
+    @DisplayName("Verificar crear una publicación con un usuario inexistente.")
+    void savePublicationPersonNotFoundException() throws Exception {
+        ObjectWriter writer = new ObjectMapper()
+                .registerModule(new JavaTimeModule()) // convertir fechas
+                .writer();
+
+        // Arrange
+        ProductDTO productDTO = new ProductDTO(1, "Mesa", "Roble", "Buena", "Verde", "con detalles");
+        PublicationDTO publicationDTO = new PublicationDTO(60, LocalDate.now(), productDTO, 2, 300.0);
+
+        String payload = writer.writeValueAsString(publicationDTO);
+
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isNotFound();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        result -> Assertions.assertTrue(result.getResolvedException() instanceof PersonNotFoundException)
+                );
+    }
+
+    @Test
+    @DisplayName("Verificar obtener lista de las ultimas publicaciones correctamente.")
+    void getLastPublicationsOk() throws Exception {
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/products/followed/{userId}/list", 55)
+                .queryParam("order", "date_asc")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // act & assert
+        MvcResult result =mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(expectedStatus)
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        SellerPListDTO sellerPListDTO = objectMapper.readValue(result.getResponse().getContentAsString(), SellerPListDTO.class);
+        //El usuario Esteban sigue a los vendedores (Antel, Movistar y Claro) y en total de las ultimas dos semanas hay 4 publicaciones.
+        Assertions.assertEquals(sellerPListDTO.getPosts().size(), 4);
+    }
+
+    @Test
+    @DisplayName("El id debe ser positivo.")
+    void getLastPublicationsConstraintViolationException() throws Exception {
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isBadRequest();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/products/followed/{userId}/list", -1)
+                .queryParam("order", "date_asc")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        result -> Assertions.assertTrue(result.getResolvedException() instanceof ConstraintViolationException)
+                );
+    }
+
+    @Test
+    @DisplayName("Verificar obtener lista de las ultimas publicaciones con un usuario inexistente.")
+    void getLastPublicationsPersonNotFoundException() throws Exception {
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isNotFound();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/products/followed/{userId}/list", 60)
+                .queryParam("order", "date_asc")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        result -> Assertions.assertTrue(result.getResolvedException() instanceof PersonNotFoundException)
+                );
+    }
+
+    @Test
+    @DisplayName("Verificar obtener lista de las ultimas publicaciones con un orden invalido.")
+    void getLastPublicationsOrderInvalidException() throws Exception {
+        // EXPECTED
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isNotFound();
+
+        // REQUEST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/products/followed/{userId}/list", 1)
+                .queryParam("order", "no_existe")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // act & assert
+        mockMvc
+                .perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        result -> Assertions.assertTrue(result.getResolvedException() instanceof OrderInvalidException)
+                );
+    }
 
 }
--- sprint II/be_java_hisp_w15_g03_Marichal_Esteban/src/test/java/com/sprint1/be_java_hisp_w15_g03/utils/TestUtilsGenerator.java
+++ sprint II/be_java_hisp_w15_g03_Marichal_Esteban/src/test/java/com/sprint1/be_java_hisp_w15_g03/utils/TestUtilsGenerator.java
@@ -1,10 +1,10 @@
 package com.sprint1.be_java_hisp_w15_g03.utils;
 
-import com.sprint1.be_java_hisp_w15_g03.model.Publication;
-import com.sprint1.be_java_hisp_w15_g03.model.RepoData;
-import com.sprint1.be_java_hisp_w15_g03.model.Seller;
-import com.sprint1.be_java_hisp_w15_g03.model.User;
+import com.sprint1.be_java_hisp_w15_g03.model.*;
+import org.hibernate.validator.internal.util.privilegedactions.LoadClass;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.List;
 public class TestUtilsGenerator {
 
     public static RepoData dataToTest(){
-        // Creacion de usuarios
+        // Creación de usuarios
         User user1 = new User(50, "Edgar");
         User user2 = new User(51, "Jaquelina");
         User user3 = new User(52, "Belen");
@@ -20,14 +20,13 @@ public class TestUtilsGenerator {
         User user5 = new User(54, "Valentina");
         User user6 = new User(55, "Esteban");
 
-        // Creacion de vendedores
+        // Creación de vendedores
         Seller seller1 = new Seller(50, "Netflix");
         Seller seller2 = new Seller(51, "Movistar");
         Seller seller3 = new Seller(52, "Claro");
         Seller seller4 = new Seller(53, "Antel");
         Seller seller5 = new Seller(54, "HBO");
         Seller seller6 = new Seller(55, "Disney");
-
 
         // Relaciones entre usuarios y vendedores
 
@@ -42,11 +41,39 @@ public class TestUtilsGenerator {
         user2.getFollowed().add(seller6);
         seller6.getFollowers().add(user2);
 
-        List<Publication> publications = new ArrayList<>();
+        // Caso de publicaciones
+        // Esteban sigue a Antel, Claro y Movistar
+        user6.getFollowed().add(seller4);
+        user6.getFollowed().add(seller2);
+        user6.getFollowed().add(seller3);
+        seller4.getFollowers().add(user6);
+        seller3.getFollowers().add(user6);
+        seller2.getFollowers().add(user6);
 
+
+        // Creación de publicaciones
+        Product product = new Product(50, "Mesa", "Madera", "MDF", "Azul", "le falta una pata");
+        Publication publication1 = new Publication(50, LocalDate.now().minus(3, ChronoUnit.WEEKS), Category.MESA, 300D, product, false, 0D);
+        Publication publication2 = new Publication(51, LocalDate.now().minus(1, ChronoUnit.WEEKS), Category.MESA, 300D, product, false, 0D);
+        Publication publication3 = new Publication(52, LocalDate.now(), Category.MESA, 300D, product, false, 0D);
+        Publication publication4 = new Publication(53, LocalDate.now().minus(3, ChronoUnit.DAYS), Category.MESA, 300D, product, false, 0D);
+        Publication publication5 = new Publication(54, LocalDate.now().minus(2, ChronoUnit.DAYS), Category.MESA, 300D, product, false, 0D);
+        Publication publication6 = new Publication(55, LocalDate.now().minus(5, ChronoUnit.WEEKS), Category.MESA, 300D, product, false, 0D);
+
+        // Relación de vendedores con las publicaciones
+        seller2.getPublications().add(publication1);
+        seller2.getPublications().add(publication2);
+        seller2.getPublications().add(publication3);
+
+        seller3.getPublications().add(publication4);
+
+        seller4.getPublications().add(publication5);
+        seller4.getPublications().add(publication6);
+
+        // Se cargan listas
+        List<Publication> publications = Arrays.asList(publication1, publication2, publication3);
         List<User> users = Arrays.asList(user1, user2, user3, user4, user5, user6);
         List<Seller> sellers = Arrays.asList(seller1, seller2, seller3, seller4, seller5, seller6);
-
 
         return new RepoData(users, sellers, publications);
     }
