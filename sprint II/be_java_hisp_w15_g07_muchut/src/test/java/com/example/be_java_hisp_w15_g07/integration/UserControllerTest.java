258399d1513d264f80f1c0a4bdcba9e3a75ef20e
Sebastian Elias Muchut <sebastian.muchut@mercadolibre.com>
2022-05-06 12:26:54 -0300

    uploading more integration tests

--- sprint II/be_java_hisp_w15_g07_muchut/src/test/java/com/example/be_java_hisp_w15_g07/integration/UserControllerTest.java
+++ sprint II/be_java_hisp_w15_g07_muchut/src/test/java/com/example/be_java_hisp_w15_g07/integration/UserControllerTest.java
@@ -6,6 +6,8 @@ import com.example.be_java_hisp_w15_g07.service.IUserService;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,12 +28,18 @@ public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    void countFollowersTest() throws Exception {
-        ObjectWriter writer =  new ObjectMapper()
+    private ObjectWriter writer;
+
+    @BeforeEach
+    void setup(){
+         this.writer =  new ObjectMapper()
                 .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
                 .writer().withDefaultPrettyPrinter();
+    }
 
+    @Test
+    @DisplayName("/users/{userId}/followers/count -> trae la cantidad de followers del usuario pasado por param")
+    void countFollowersTest() throws Exception {
         // Arrange
         FollowersCountDTO count = new FollowersCountDTO(2, "User 2", 2);
         String countJson = writer.writeValueAsString(count);
@@ -51,6 +59,60 @@ public class UserControllerTest {
                         expectedStatus,
                         expectedJson,
                         expectedContentType
+                );
+    }
+
+    @Test
+    @DisplayName("/users/{userId}/followers/count -> lanza excepción cuando no encuentra el usuario")
+    void countFollowersFailsTest() throws Exception {
+        // Request
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/{userId}/followers/count", 100);
+
+        // Expected
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isNotFound();
+        ResultMatcher expectedContentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+        // Act and Asser
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus,
+                        expectedContentType
+                );
+    }
+
+    @Test
+    @DisplayName("/{userId}/follow/{userIdToFollow} -> devuelve un OK al intentar seguir un usuario")
+    void followUserTest() throws Exception {
+        // Request
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}",2, 3);
+
+        // Expected
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().isOk();
+
+        // Act and Assert
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus
+                );
+
+    }
+
+    @Test
+    @DisplayName("/{userId}/follow/{userIdToFollow} -> lanza una excepción si el usuario no existe")
+    void followUserFailsTest() throws Exception {
+        // Request
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}",-2, 3);
+
+        // Expected
+        ResultMatcher expectedStatus = MockMvcResultMatchers.status().is4xxClientError();
+
+        // Act and Assert
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        expectedStatus
                 );
     }
 }
