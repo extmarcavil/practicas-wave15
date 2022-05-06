d57ecdf52e17dc9b6e439ce77ccaa7a5b56fee3d
Diana1501 <diana.mhurtadog@gmail.com>
2022-05-06 09:22:24 -0500

    PostCreatedDTO test 100

--- sprint II/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/DTO/Response/PostCreatedDTO.java
+++ sprint II/be_java_hisp_w15_g10/src/main/java/com/sprint/be_java_hisp_w15_g10/DTO/Response/PostCreatedDTO.java
@@ -3,10 +3,8 @@ package com.sprint.be_java_hisp_w15_g10.DTO.Response;
 import lombok.*;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class PostCreatedDTO {
     private String message;
 }
