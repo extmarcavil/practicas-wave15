a4c650ff19e5b66f51a76979333082eca3ef8f82
mfranciscog <marcos.giaccaglia@mercadolibre.com>
2022-05-06 20:28:17 -0300

    cambio de nombre clase test de integracion

--- sprint II/be_java_hisp_w15_g05_giaccaglia/src/test/java/com/example/be_java_hisp_w15_g05/integration_test/ControllersIntegrationTests.java
+++ sprint II/be_java_hisp_w15_g05_giaccaglia/src/test/java/com/example/be_java_hisp_w15_g05/integration_test/ControllersIntegrationTests.java
@@ -23,7 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class FollowsControllerIntegrationTest {
+public class ControllersIntegrationTests {
 
     @Autowired
     MockMvc mockMvc;
