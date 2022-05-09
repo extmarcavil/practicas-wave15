d8b71c69c89d0cafd3fbe3a4a47c3095d530ca23
Ramiro Vera <ramiro.vera@mercadolibre.com>
2022-05-09 13:49:55 -0300

    Add grupal directory

---
+++ sprint II/be_java_hisp_w15_g09/src/main/java/sprint2/socialmeli/repository/PostRepository.java
@@ -0,0 +1,37 @@
+package sprint2.socialmeli.repository;
+
+import lombok.Getter;
+import org.springframework.stereotype.Repository;
+import sprint2.socialmeli.model.Post;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@Repository
+public class PostRepository implements IPostRepository {
+    private final Map<Integer, Post> postList;
+    private Integer postId;
+
+    public PostRepository() {
+        this.postList = new HashMap<>();
+        postId = 0;
+    }
+
+    @Override
+    public Integer save(Post post) {
+        this.postList.put(++postId, post);
+        post.setPostId(postId);
+        return postId;
+    }
+
+    @Override
+    public List<Post> getListOfPostOfUser(int followedIDToSearch) {
+        return postList.values()
+                .stream()
+                .filter((Post p) -> p.hasUserID(followedIDToSearch))
+                .collect(Collectors.toList());
+    }
+}
