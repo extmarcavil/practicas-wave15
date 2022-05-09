47bdb6befc1d1851b8bdf5f605f86cf589d88c35
sofiacotmeli <sofia.cot@mercadolibre.com>
2022-05-09 09:55:41 -0300

    agrego ejercicio starwars

---
+++ spring/starwars/src/main/java/com/example/starwars/dto/CharacterDTO.java
@@ -0,0 +1,24 @@
+package com.example.starwars.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CharacterDTO {
+  private String name;
+  private String hair_color;
+  private String skin_color;
+  private String eye_color;
+  private String birth_year;
+  private String gender;
+  private String homeworld;
+  private String species;
+  private Integer height;
+  private Integer mass;
+
+}
