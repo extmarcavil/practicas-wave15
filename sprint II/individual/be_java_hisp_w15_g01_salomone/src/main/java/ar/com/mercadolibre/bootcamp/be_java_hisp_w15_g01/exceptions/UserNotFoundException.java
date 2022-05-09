6093083d224c87e1090be8630aa2c5d2f8b17e53
AnaSalomone <ana.salomone@mercadolibre.com>
2022-05-09 11:46:59 -0300

    add integration tests0

--- sprint II/individual/be_java_hisp_w15_g01_salomone/src/main/java/ar/com/mercadolibre/bootcamp/be_java_hisp_w15_g01/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ar.com.mercadolibre.bootcamp.be_java_hisp_w15_g01.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "The user does not exists")
-public class UserNotFoundException extends RuntimeException {
-}
