3d5571af57afc62dc39625d533d90e3472f41051
vparula <valentina.parula@mercadolibre.com>
2022-05-10 17:04:27 -0300

    Agrego ejercicios del dia 1 DB

---
+++ bases de datos/mysql/moviesConsulta.sql
@@ -0,0 +1,13 @@
+select * from movies;
+select first_name,last_name,rating from actors;
+select title as 'Titulo' from series;
+select first_name,last_name from actors where rating>7.5;
+select title, rating, awards from movies where rating>7.5 and awards>2;
+select title, rating from movies order by rating asc;
+select title from movies order by id limit 3;
+select title, rating from movies order by rating desc limit 5;
+select * from actors limit 10;
+select title, rating from movies where title like '%%Toy Story%%';
+select * from actors where first_name like 'Sam%%';
+select title from movies where release_date between '2004-01-01' and '2008-12-31';
+select title from movies where rating>3 and awards>1 and release_date between '1988-01-01' and '2009-12-31';
