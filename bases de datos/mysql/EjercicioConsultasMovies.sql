9fd5b2abf06c5ff9f51122671b84fa863cb0fe0c
Maria Belen Tracchia <mariabelen.tracchia@mercadolibre.com>
2022-05-11 09:27:34 -0300

    Ejercicio consultas movies

---
+++ bases de datos/mysql/EjercicioConsultasMovies.sql
@@ -0,0 +1,13 @@
+SELECT * FROM movies_db.actors;
+SELECT first_name, last_name,rating FROM actors;
+SELECT title AS Titulo FROM series AS Series;
+SELECT first_name, last_name FROM actors where rating >7.5;
+SELECT title,rating,awards FROM movies WHERE rating >7.5 AND awards >2; 
+SELECT title, rating FROM movies ORDER BY rating;
+SELECT title FROM movies LIMIT 3;
+SELECT * FROM movies ORDER BY rating DESC LIMIT 5;
+SELECT * FROM actors LIMIT 10;
+SELECT title, rating FROM movies WHERE title LIKE "Toy Story%";
+SELECT * FROM actors WHERE first_name LIKE "Sam%";
+SELECT title FROM movies WHERE release_date BETWEEN "2004-01-01 00:00:00" AND "2008-12-31 23:59:59";
+SELECT title FROM movies WHERE rating > 3 AND awards >1 AND release_date BETWEEN "1988-01-01" and "2009-12-31" ORDER BY rating;
