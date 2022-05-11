8660ebd5f651bf8c65cf83e19fabc31dc58a28e4
Diana1501 <diana.mhurtadog@gmail.com>
2022-05-11 14:06:03 -0500

    Consultas SQL parte 2

---
+++ bases de datos/mysql/querys_movies_db.sql
@@ -0,0 +1,14 @@
+USE movies_db;
+SELECT * FROM movies;
+SELECT first_name, last_name, rating FROM actors;
+SELECT title as 'Título' FROM series serie;
+SELECT first_name, last_name FROM actors WHERE rating > 7.5;
+SELECT title, rating, awards FROM movies WHERE rating > 7.5 AND awards > 2;
+SELECT title, rating FROM movies ORDER BY rating;
+SELECT title FROM movies LIMIT 3;
+SELECT * FROM movies ORDER BY rating DESC LIMIT 5;
+SELECT * FROM actors LIMIT 10;
+SELECT title, rating FROM movies WHERE title = 'Toy Story';
+SELECT * FROM actors WHERE first_name LIKE 'Sam%';
+SELECT title FROM movies WHERE release_date BETWEEN '2004-01-01' AND '2008-12-31';
+SELECT title FROM movies WHERE rating > 3 AND awards >= 1 AND release_date BETWEEN '1988-01-01' AND '2009-12-31' ORDER BY rating;
