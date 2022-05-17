51d332d9c4d9e9d1d2763e224fc79bc8831c55e3
Tomas Ravelli <tomas.ravelli@mercadolibre.com>
2022-05-17 14:44:26 -0300

    Ejercicio miniseries: falta probar que ande por que lo movi de ubicacion

---
+++ impl base de datos/miniseries/src/main/java/com/example/miniseries/repository/IMiniSerieRepository.java
@@ -0,0 +1,10 @@
+package com.example.miniseries.repository;
+
+import com.example.miniseries.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+
+}
--- impl base de datos/src/main/java/com/example/miniseries/repository/IMiniSerieRepository.java
+++
@@ -1,2 +0,0 @@
-package com.example.miniseries.repository;public interface IMiniSerieRepository {
-}
