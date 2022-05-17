a61da576fc954763854bc344fd74844fae818924
lmacbit <lorena.maciel@mercadolibre.com>
2022-05-17 12:42:21 -0300

    Ejercicio práctico MiniSerie de Configurando JPA

---
+++ impl base de datos/MiniSeries/src/main/java/com/bootcamp/miniseries/repository/IMiniserieRepository.java
@@ -0,0 +1,9 @@
+package com.bootcamp.miniseries.repository;
+
+import com.bootcamp.miniseries.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniserieRepository  extends JpaRepository<MiniSerie, Long> {
+}
---
+++ impl base de datos/MiniSeries/src/test/java/com/bootcamp/miniseries/MiniseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.miniseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
