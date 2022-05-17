952d0546bdcbb582282bd275e80d8e3e55c96229
josnnez <josue.nunez@mercadolibre.com.co>
2022-05-17 11:08:39 -0500

    use of spring JPA dependency, h2 with embedded db

---
+++ impl base de datos/MiniSeries/src/main/java/com/example/miniseries/model/MiniSerie.java
@@ -0,0 +1,20 @@
+package com.example.miniseries.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// @Data
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private double rating;
+    private int amountAwards;
+
+}
