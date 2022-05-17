757ae2e40feb6a9f1193ed6483182a68ba828a92
IFerrante-meli <ignacio.ferrante@mercadolibre.com>
2022-05-17 16:42:58 -0300

    agregando desarrollo de martin

---
+++ impl base de datos/TestCase/src/main/java/com/meli/TestCase/controller/TesterController.java
@@ -0,0 +1,38 @@
+package com.meli.TestCase.controller;
+
+import com.meli.TestCase.Dto.ReqTestCaseDto;
+import com.meli.TestCase.Dto.ResPostTestDto;
+import com.meli.TestCase.model.TestCase;
+import com.meli.TestCase.service.ITestCaseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/testcases")
+public class TesterController {
+
+    private final ITestCaseService service;
+
+    public TesterController(ITestCaseService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<ResPostTestDto> crearRegistroTest(@RequestBody ReqTestCaseDto test){
+        return new ResponseEntity<>(service.guardar(test), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<TestCase>> obtenerTodos(){
+        return new ResponseEntity<>(service.obtenerTodos(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TestCase> obtenerUnoPorId(@PathVariable Long id){
+        return new ResponseEntity<>(service.obtenerPorId(id),HttpStatus.OK);
+    }
+
+}
---
+++ impl base de datos/TestCase/src/main/java/com/meli/TestCase/service/ImpTestCaseService.java
@@ -0,0 +1,40 @@
+package com.meli.TestCase.service;
+
+import com.meli.TestCase.Dto.ReqTestCaseDto;
+import com.meli.TestCase.Dto.ResPostTestDto;
+import com.meli.TestCase.model.TestCase;
+import com.meli.TestCase.repository.ITestCaseRespository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ImpTestCaseService implements ITestCaseService{
+    private final ITestCaseRespository repo;
+
+    public ImpTestCaseService(ITestCaseRespository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public ResPostTestDto guardar(ReqTestCaseDto test){
+        TestCase t = new TestCase(test.getDesc(),
+                test.isTested(),
+                test.isPassed(),
+                test.getNumber_of_tries(),
+                test.getLastUpdate());
+        repo.save(t);
+
+        return new ResPostTestDto(t.getDescription(),
+                t.isTested(),t.isPassed(),t.getNumber_of_tries(),t.getLastUpdate(),"todo ok");
+    }
+    @Override
+    public List<TestCase> obtenerTodos(){
+        return repo.findAll();
+    }
+    @Override
+    public TestCase obtenerPorId(Long id){
+        return repo.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+}
