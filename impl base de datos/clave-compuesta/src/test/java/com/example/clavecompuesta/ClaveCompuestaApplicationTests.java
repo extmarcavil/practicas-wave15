889dcc22c336e9697d6d25aadf7f8daacb46c0e4
Lucas Jimenez <lucas.jimenez@mercadolibre.com>
2022-05-18 18:31:59 -0300

    Subo ejercicio guia y ejercicio opcional

---
+++ impl base de datos/clave-compuesta/src/test/java/com/example/clavecompuesta/ClaveCompuestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clavecompuesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaveCompuestaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ impl base de datos/relaciones/src/test/java/com/example/relaciones/RelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
