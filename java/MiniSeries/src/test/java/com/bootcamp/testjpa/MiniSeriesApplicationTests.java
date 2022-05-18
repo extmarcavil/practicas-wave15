4e842cc6f29e1414ce385047661097e77f424719
Juan Chaparro <juan.chaparro@mercadolibre.com.co>
2022-05-18 13:53:15 -0500

    Subiendo los aportes faltantes

---
+++ java/MiniSeries/src/test/java/com/bootcamp/testjpa/MiniSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.testjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ java/TestCase/src/main/java/com/meli/TestCase/controller/TesterController.java
@@ -0,0 +1,53 @@
+package com.meli.TestCase.controller;
+
+import com.meli.TestCase.Dto.ReqTestCaseDto;
+import com.meli.TestCase.Dto.ResPostTestDto;
+import com.meli.TestCase.model.TestCase;
+import com.meli.TestCase.service.ITestCaseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/testcases")
+public class TesterController {
+
+    private ITestCaseService service;
+
+    public TesterController(ITestCaseService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<ResPostTestDto> crearRegistroTest(@RequestBody ReqTestCaseDto test){
+        return new ResponseEntity<>(service.guardar(test), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<TestCase>> obtenerTodos(){
+        return new ResponseEntity<>(service.obtenerTodos(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TestCase> obtenerUnoPorId(@PathVariable Long id){
+        return new ResponseEntity<>(service.obtenerPorId(id),HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResPostTestDto> editarRegistroTest(@PathVariable Long id,
+                                                             @RequestParam boolean test_passed){
+        return new ResponseEntity<>(service.editar(id, test_passed), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> crearRegistroTest(@PathVariable Long id){
+        return new ResponseEntity<>(service.eliminar(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/fecha")
+    public ResponseEntity<List<TestCase>> obtenerTodosPorFecha(@RequestParam String last_update){
+        return new ResponseEntity<>(service.obtenerTodosPorFecha(last_update),HttpStatus.OK);
+    }
+}
