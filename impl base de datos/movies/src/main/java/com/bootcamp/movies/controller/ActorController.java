533e0d0bb24ca2e7e69351ff2344ca56b04051a2
lmacbit <lorena.maciel@mercadolibre.com>
2022-05-19 11:21:24 -0300

    Update ActorController.java

--- impl base de datos/movies/src/main/java/com/bootcamp/movies/controller/ActorController.java
+++ impl base de datos/movies/src/main/java/com/bootcamp/movies/controller/ActorController.java
@@ -5,9 +5,7 @@ import com.bootcamp.movies.model.Actor;
 import com.bootcamp.movies.service.ActorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,18 @@ public class ActorController {
     @GetMapping
     @RequestMapping("/who_have_favorite_movie")
     public ResponseEntity<List<RespActorDTO>> getActorsWhoHaveFavoriteMovie() {
-        return new ResponseEntity<>(actorService.getAllActorWhoHaveFavoriteMovie(), HttpStatus.OK);
+        return new ResponseEntity<>(actorService.getAllActorsWhoHaveFavoriteMovie(), HttpStatus.OK);
+    }
+
+    @GetMapping
+    @RequestMapping("/rating")
+    public ResponseEntity<List<RespActorDTO>> getAllActorsWhoHaveRatingGreaterThan(@RequestParam("greater_than") Double rating) {
+        return new ResponseEntity<>(actorService.getAllActorsWhoHaveRatingGreaterThan(rating), HttpStatus.OK);
+    }
+
+    @GetMapping
+    @RequestMapping("/movie")
+    public ResponseEntity<List<RespActorDTO>> getAllActorsWhoHaveFavoriteMovie(@RequestParam("movie_name") String name) {
+        return new ResponseEntity<>(actorService.getAllActorsWhoWorkingInTheMovieByName(name), HttpStatus.OK);
     }
 }
