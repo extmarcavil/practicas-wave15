aa4303a2307ea29ebc274569bf2f1a5c1bb955b3
Roy-Martinez <roy.martinez@mercadolibre.com.co>
2022-05-21 11:51:44 -0500

    Implementacion BD Ejercicios SQL & ES

---
+++ impl base de datos/claveCompuesta/src/main/java/com/example/claveCompuesta/model/CompraKey.java
@@ -0,0 +1,12 @@
+package com.example.claveCompuesta.model;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class CompraKey implements Serializable {
+
+    private Long clienteId;
+
+    private LocalDate fecha;
+}
---
+++ impl base de datos/claveCompuesta/src/test/java/com/example/claveCompuesta/ClaveCompuestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claveCompuesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaveCompuestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
