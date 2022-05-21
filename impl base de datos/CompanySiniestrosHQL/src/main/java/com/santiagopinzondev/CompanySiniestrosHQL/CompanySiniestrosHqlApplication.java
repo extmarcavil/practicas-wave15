07b4f9233519e6cb23f00553a5dd2d705c14f833
santiagopinzondev <santiago.pinzon@mercadolibre.com.co>
2022-05-21 10:25:10 -0500

    add exercise of hql vehiculo and siniestros

---
+++ impl base de datos/CompanySiniestrosHQL/src/main/java/com/santiagopinzondev/CompanySiniestrosHQL/CompanySiniestrosHqlApplication.java
@@ -0,0 +1,13 @@
+package com.santiagopinzondev.CompanySiniestrosHQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanySiniestrosHqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanySiniestrosHqlApplication.class, args);
+	}
+
+}
---
+++ impl base de datos/CompanySiniestrosHQL/src/test/java/com/santiagopinzondev/CompanySiniestrosHQL/CompanySiniestrosHqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagopinzondev.CompanySiniestrosHQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySiniestrosHqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
