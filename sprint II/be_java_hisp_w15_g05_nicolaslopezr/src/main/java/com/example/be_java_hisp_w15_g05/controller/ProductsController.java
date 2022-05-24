3b73b3138f8235a0e3dc76809242c688586d660c
nicolas_lopezre <nicolasalejandro.lopez@mercadolibre.com>
2022-05-24 08:49:18 -0300

    prueba de subir

---
+++ sprint II/be_java_hisp_w15_g05_nicolaslopezr/src/main/java/com/example/be_java_hisp_w15_g05/controller/ProductsController.java
@@ -0,0 +1,54 @@
+package com.example.be_java_hisp_w15_g05.controller;
+
+import com.example.be_java_hisp_w15_g05.dto.PostDTO;
+import com.example.be_java_hisp_w15_g05.dto.ResCreatePostDTO;
+import com.example.be_java_hisp_w15_g05.dto.ResPostListDTO;
+import com.example.be_java_hisp_w15_g05.service.IProductsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    private IProductsService productsService;
+
+    public ProductsController(IProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    /**
+     *
+     * @param postDTO publicación a agregar.
+     * @return
+     */
+    @PostMapping("/post")
+    public ResponseEntity<ResCreatePostDTO> createPost(@Valid @RequestBody PostDTO postDTO) {
+        return new ResponseEntity<>(productsService.createPost(postDTO), HttpStatus.OK);
+    }
+
+    /**
+     *
+     * @param userId id del usuario a buscar los post de los vendedores a los cual sigue
+     * @param order debe ser 'date_asc' | 'date_desc' | ''
+     * @return
+     */
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<ResPostListDTO> queryPost(
+            @Positive(message = FollowsController.userIDPositive)
+            @NotNull(message = FollowsController.userIDNotNull)
+            @PathVariable int userId,
+            @RequestParam(required = false, defaultValue = "") String order) {
+        return new ResponseEntity<>(productsService.getPostFollowed(userId, order), HttpStatus.OK);
+
+    }
+}
