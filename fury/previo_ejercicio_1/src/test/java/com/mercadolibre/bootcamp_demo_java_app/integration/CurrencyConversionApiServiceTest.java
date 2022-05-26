29075f43bc8eb0382e4037e2b03a94248b08b7a8
Lucas <lucas.fedele@mercadolibre.com>
2022-05-26 17:59:27 -0300

    ejercicio 1 fury

---
+++ fury/previo_ejercicio_1/src/test/java/com/mercadolibre/bootcamp_demo_java_app/integration/CurrencyConversionApiServiceTest.java
@@ -0,0 +1,31 @@
+package com.mercadolibre.bootcamp_demo_java_app.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mercadolibre.bootcamp_demo_java_app.api.services.CurrencyConversionApiService;
+import com.mercadolibre.bootcamp_demo_java_app.dtos.CurrencyConversionDTO;
+import com.mercadolibre.bootcamp_demo_java_app.dtos.CurrencyEnum;
+import com.mercadolibre.bootcamp_demo_java_app.util.MockUtils;
+import com.mercadolibre.restclient.exception.ParseException;
+import com.mercadolibre.restclient.exception.RestException;
+
+public class CurrencyConversionApiServiceTest extends IntegrationTest {
+	@Autowired
+	CurrencyConversionApiService currenctConversionApiService;
+	
+	@Test
+	public void testGetCurrencyConversion() throws URISyntaxException, IOException, ParseException, RestException {
+		CurrencyEnum sourceCurrency = CurrencyEnum.UYU;
+		MockUtils.mockCurrencyConversion(sourceCurrency);
+		Double expectedRatio = Double.valueOf("0.02247191");
+		CurrencyConversionDTO expectedCurrenctConversion = new CurrencyConversionDTO(expectedRatio, CurrencyEnum.UYU, CurrencyEnum.USD);
+		
+		assertEquals(expectedCurrenctConversion, currenctConversionApiService.getCurrencyConversion(sourceCurrency, CurrencyEnum.USD));
+	}	
+}
