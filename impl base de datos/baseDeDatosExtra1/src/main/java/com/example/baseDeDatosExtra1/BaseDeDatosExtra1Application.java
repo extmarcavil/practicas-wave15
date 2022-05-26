c80b44c98704e1743255501f0b4b78fa2b3fbce2
Sebastian Elias Muchut <sebastian.muchut@mercadolibre.com>
2022-05-26 18:25:35 -0300

    intro Fury - consumo de APIs

---
+++ impl base de datos/baseDeDatosExtra1/src/main/java/com/example/baseDeDatosExtra1/BaseDeDatosExtra1Application.java
@@ -0,0 +1,13 @@
+package com.example.baseDeDatosExtra1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDeDatosExtra1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDeDatosExtra1Application.class, args);
+	}
+
+}
---
+++ impl base de datos/baseDeDatosExtra1/src/test/java/com/example/baseDeDatosExtra1/BaseDeDatosExtra1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseDeDatosExtra1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDeDatosExtra1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
