d7a2cc3cb28bd82eeadc4bd4909d96b6c1b11aed
Maria Belen Tracchia <mariabelen.tracchia@mercadolibre.com>
2022-05-26 16:00:11 -0300

    Fury

--- Fury/previo_ejercicio_1/src/main/java/com/mercadolibre/bootcamp_demo_java_app/dtos/CurrencyEnum.java
+++
@@ -1,5 +0,0 @@
-package com.mercadolibre.bootcamp_demo_java_app.dtos;
-
-public enum CurrencyEnum {
-    ARS, BOB, BRL, CLF, CLP, COP, CRC, CUC, CUP, DOP, EUR, GTQ, HNL, MXN, NIO, PAB, PEN, PYG, USD, UYU, VEF, VES;
-}
